efc94e22a365fb5fa6d8e345d1d99da7f333de34
oowtl <oowtl7425@gmail.com>
2025-01-07 22:16:09 +0900

    feat : TaskBoardFile Entity
    - TaskBoardFile Entity 구현
    - Join 컬럼 이후 구현 예정

---
+++ module-domain/src/main/java/com/checkping/domain/board/TaskBoardFile.java
@@ -0,0 +1,43 @@
+package com.checkping.domain.board;
+
+import com.checkping.domain.BaseEntity;
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.ToString;
+
+@Getter
+@ToString
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+@Table(name = "task_board_file")
+@Entity
+public class TaskBoardFile extends BaseEntity {
+    /*
+    id : 업무 관리 게시글 첨부 파일 아이디
+    name : 업무 관리 게시글 첨부 파일 명
+    url : 업무 관리 게시글 첨부 파일 url
+    size : 업무 관리 게시글 첨부 용량
+     */
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id", nullable = false)
+    private Long id;
+
+    @Column(name = "name", nullable = false)
+    private String name;
+
+    @Column(name = "url", nullable = false)
+    private String url;
+
+    @Column(name = "size", nullable = false)
+    private String size;
+}
