c7f3b0c4a5f4f8fc932d181b6cd40d3838785720
oowtl <oowtl7425@gmail.com>
2025-01-08 22:31:41 +0900

    feat : TaskBoardStore 추가
    - store 메서드 추가

---
+++ module-infra/src/main/java/com/checkping/infra/repository/project/TaskBoardStoreImpl.java
@@ -0,0 +1,17 @@
+package com.checkping.infra.repository.project;
+
+import com.checkping.domain.board.TaskBoard;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Component;
+
+@Component
+@RequiredArgsConstructor
+public class TaskBoardStoreImpl implements TaskBoardStore {
+
+    private final TaskBoardRepository taskBoardRepository;
+
+    @Override
+    public TaskBoard store(TaskBoard taskBoard) {
+        return taskBoardRepository.save(taskBoard);
+    }
+}
