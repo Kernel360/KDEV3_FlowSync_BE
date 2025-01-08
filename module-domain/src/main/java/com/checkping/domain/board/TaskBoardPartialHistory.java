92a62c682a2ef28ae35add9a706a4ffdf2ec2792
oowtl <oowtl7425@gmail.com>
2025-01-08 11:31:17 +0900

    feat : TaskBoardPartialHistory JoinColumn 추가
    TaskBoardPartialHistory JoinColumn
    1. taskBoard 추가
    특이사항
    1. member entity 구현 후 joinColumn 추가 예정

--- module-domain/src/main/java/com/checkping/domain/board/TaskBoardPartialHistory.java
+++ module-domain/src/main/java/com/checkping/domain/board/TaskBoardPartialHistory.java
@@ -3,9 +3,12 @@ package com.checkping.domain.board;
 import com.checkping.domain.BaseEntity;
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
+import jakarta.persistence.FetchType;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
 import jakarta.persistence.Table;
 import java.time.LocalDateTime;
 import lombok.AllArgsConstructor;
@@ -29,6 +32,7 @@ public class TaskBoardPartialHistory extends BaseEntity {
     currentValue : 현재 값
     previousValue : 변경 이전 값
     updatedAt : 수정 일시
+    taskBoard : 업무 관리 게시글 (join)
      */
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
@@ -47,4 +51,8 @@ public class TaskBoardPartialHistory extends BaseEntity {
     @LastModifiedDate
     @Column (name = "updated_at", nullable = false)
     private LocalDateTime updatedAt;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "task_board_id", nullable = false)
+    private TaskBoard taskBoard;
 }
