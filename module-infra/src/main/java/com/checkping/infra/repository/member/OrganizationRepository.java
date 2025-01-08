fc5685b1cd972dd3c1a544737c8dca12e5357189
jaseongkim <jaseong4281@gmail.com>
2025-01-08 17:42:16 +0900

    feat: 조회 기능 레포 및 테스트 코드 구현
    ID를 통한 조회 추가
    타입별 조회 기능 추가
    서비스 계층은 미구현 상태(구현 예정)
    
    Issue #71

--- module-infra/src/main/java/com/checkping/infra/repository/member/OrganizationRepository.java
+++ module-infra/src/main/java/com/checkping/infra/repository/member/OrganizationRepository.java
@@ -3,11 +3,16 @@ package com.checkping.infra.repository.member;
 import com.checkping.domain.member.Organization;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
 import java.util.Optional;
 import java.util.UUID;
 
 public interface OrganizationRepository extends JpaRepository<Organization, UUID> {
 
     Optional<Organization> findByNameAndType(String name, Organization.Type type);
+
+    Optional<Organization> findById(UUID id);
+
+    List<Organization> findByType(Organization.Type type);
 
 }
--- module-infra/src/test/java/com/checkping/infra/OrganizationRepositoryTests.java
+++ module-infra/src/test/java/com/checkping/infra/OrganizationRepositoryTests.java
@@ -2,22 +2,45 @@ package com.checkping.infra;
 
 import com.checkping.domain.member.Organization;
 import com.checkping.infra.repository.member.OrganizationRepository;
+import lombok.extern.log4j.Log4j2;
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 
 import java.time.LocalDateTime;
 import java.util.List;
+import java.util.Optional;
+import java.util.UUID;
 
 @SpringBootTest
+@Log4j2
 class OrganizationRepositoryTests {
 
     @Autowired
     private OrganizationRepository organizationRepository;
 
     @Test
-    void testCreateAndGetAllOrganization(){
-        for(int i = 0; i<5; i++) {
+    void testCreateOrganization() {
+        for (int i = 0; i < 5; i++) {
+            Organization organization = Organization.builder()
+                    .name("kernel")
+                    .type(Organization.Type.CUSTOMER)
+                    .status(Organization.Status.ACTIVE)
+                    .regAt(LocalDateTime.now())
+                    .build();
+
+            Organization savedOrganization = organizationRepository.save(organization);
+            Assertions.assertNotNull(savedOrganization.getId());
+        }
+    }
+
+    @Test
+    void testCreateAndGetOrganization() {
+
+        UUID id = null;
+
+        for (int i = 0; i < 5; i++) {
             Organization organization = Organization.builder()
                     .name("kernel")
                     .type(Organization.Type.CUSTOMER)
@@ -26,9 +49,66 @@ class OrganizationRepositoryTests {
                     .build();
 
             organizationRepository.save(organization);
+            if (i == 4) {
+                id = organization.getId();
+            }
+        }
+        Optional<Organization> result = organizationRepository.findById(id);
+        Assertions.assertTrue(result.isPresent());
+    }
+
+    @Test
+    void testCreateAndGetByTypeOrganization() {
+        for (int i = 0; i < 10; i++) {
+            Organization organization;
+            if (i < 5) {
+                organization = Organization.builder()
+                        .name("kernel")
+                        .type(Organization.Type.CUSTOMER)
+                        .status(Organization.Status.ACTIVE)
+                        .regAt(LocalDateTime.now())
+                        .build();
+            } else {
+                organization = Organization.builder()
+                        .name("kernel")
+                        .type(Organization.Type.DEVELOPER)
+                        .status(Organization.Status.ACTIVE)
+                        .regAt(LocalDateTime.now())
+                        .build();
+            }
+            organizationRepository.save(organization);
+        }
+        List<Organization> organizationsDev = organizationRepository.findByType(Organization.Type.DEVELOPER);
+        List<Organization> organizationsCs = organizationRepository.findByType(Organization.Type.CUSTOMER);
+
+        Assertions.assertNotNull(organizationsDev);
+        Assertions.assertNotNull(organizationsCs);
+    }
+
+    @Test
+    void testCreateAndGetAllOrganization() {
+        for (int i = 0; i < 10; i++) {
+            Organization organization;
+            if (i < 5) {
+                organization = Organization.builder()
+                        .name("kernel")
+                        .type(Organization.Type.CUSTOMER)
+                        .status(Organization.Status.ACTIVE)
+                        .regAt(LocalDateTime.now())
+                        .build();
+            } else {
+                organization = Organization.builder()
+                        .name("kernel")
+                        .type(Organization.Type.DEVELOPER)
+                        .status(Organization.Status.ACTIVE)
+                        .regAt(LocalDateTime.now())
+                        .build();
+            }
+            organizationRepository.save(organization);
         }
         List<Organization> organizations = organizationRepository.findAll();
-        System.out.println(organizations);
+
+        Assertions.assertNotNull(organizations);
     }
 
 }
