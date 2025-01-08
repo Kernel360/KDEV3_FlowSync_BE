b09f6c4edad774841201563157687d6e86ad0ae4
oowtl <oowtl7425@gmail.com>
2025-01-08 11:32:45 +0900

    feat : TaskBoardFile JoinColumn 추가
    TaskBoardFile JoinColumn
    1. taskBoard 추가

--- module-domain/src/main/java/com/checkping/domain/board/TaskBoardFile.java
+++ module-domain/src/main/java/com/checkping/domain/board/TaskBoardFile.java
@@ -3,9 +3,12 @@ package com.checkping.domain.board;
 import com.checkping.domain.BaseEntity;
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
+import jakarta.persistence.FetchType;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
 import jakarta.persistence.Table;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
@@ -26,6 +29,7 @@ public class TaskBoardFile extends BaseEntity {
     name : 업무 관리 게시글 첨부 파일 명
     url : 업무 관리 게시글 첨부 파일 url
     size : 업무 관리 게시글 첨부 용량
+    taskBoard : 업무 관리 게시글 (join)
      */
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
@@ -40,4 +44,8 @@ public class TaskBoardFile extends BaseEntity {
 
     @Column(name = "size", nullable = false)
     private String size;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "task_board_id", nullable = false)
+    private TaskBoard taskBoard;
 }
