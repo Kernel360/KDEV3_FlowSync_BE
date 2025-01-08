be5c76b1851c3f2f107fbf908620161b35ad7456
oowtl <oowtl7425@gmail.com>
2025-01-08 11:34:02 +0900

    feat : TaskBoardLink JoinColumn 추가
    TaskBoardLink JoinColumn
    1. taskBoard 추가

--- module-domain/src/main/java/com/checkping/domain/board/TaskBoardLink.java
+++ module-domain/src/main/java/com/checkping/domain/board/TaskBoardLink.java
@@ -3,9 +3,12 @@ package com.checkping.domain.board;
 import com.checkping.domain.BaseEntity;
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
+import jakarta.persistence.FetchType;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
 import jakarta.persistence.Table;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
@@ -25,6 +28,7 @@ public class TaskBoardLink extends BaseEntity {
     id : 업무 관리 게시글 첨부 링크 아이디
     name : 업무 관리 게시글 첨부 링크 이름
     url : 업무 관리 게시글 첨부 링크 URL
+    taskBoard : 업무 관리 게시글 (join)
      */
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
@@ -36,4 +40,8 @@ public class TaskBoardLink extends BaseEntity {
 
     @Column(name = "url", nullable = false)
     private String url;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "task_board_id", nullable = false)
+    private TaskBoard taskBoard;
 }
