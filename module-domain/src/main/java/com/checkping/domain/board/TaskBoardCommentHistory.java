47d1d04788855e309d2e7dce126208f03f17c566
oowtl <oowtl7425@gmail.com>
2025-01-08 11:36:19 +0900

    feat : TaskBoardCommentHistory JoinColumn 추가
    TaskBoardCommentHistory JoinColumn
    1. taskBoardComment 추가

--- module-domain/src/main/java/com/checkping/domain/board/TaskBoardCommentHistory.java
+++ module-domain/src/main/java/com/checkping/domain/board/TaskBoardCommentHistory.java
@@ -2,9 +2,12 @@ package com.checkping.domain.board;
 
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
+import jakarta.persistence.FetchType;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
 import jakarta.persistence.Table;
 import java.time.LocalDateTime;
 import lombok.AllArgsConstructor;
@@ -26,6 +29,7 @@ public class TaskBoardCommentHistory {
     id : 업무 관리 게시글 댓글 이력 아이디
     content : 댓글 내용
     updateAt : 수정 일시
+    taskBoardComment : 업무 관리 게시글 댓글
      */
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
@@ -38,4 +42,8 @@ public class TaskBoardCommentHistory {
     @LastModifiedDate
     @Column(name = "update_at", nullable = false)
     private LocalDateTime updateAt;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "task_board_comment_id", nullable = false)
+    private TaskBoardComment taskBoardComment;
 }
