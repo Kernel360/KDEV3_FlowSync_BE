a740fffef0a684de8506773c1a5c5556665eec0f
oowtl <oowtl7425@gmail.com>
2025-01-09 15:02:08 +0900

    feat : BaseException 추가에 따른 GlobalExceptionHandler 수정
    - BaseException 에 대한 ExceptionHandler 추가
    - 각 에러에 대한 로그메시지 출력 메서드 추가
    - CustomException 은 추후에 삭제할 예정이기 때문에 로그를 추가하지 않음

--- module-api/src/main/java/com/checkping/api/exceptionhandler/GlobalExceptionHandler.java
+++ module-api/src/main/java/com/checkping/api/exceptionhandler/GlobalExceptionHandler.java
@@ -1,11 +1,14 @@
 package com.checkping.api.exceptionhandler;
 
 import com.checkping.common.enums.ErrorCode;
+import com.checkping.common.exception.BaseException;
 import com.checkping.common.exception.CustomException;
 import com.checkping.common.response.BaseResponse;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 
+@Slf4j
 @RestControllerAdvice
 public class GlobalExceptionHandler {
 
@@ -14,8 +17,15 @@ public class GlobalExceptionHandler {
         return BaseResponse.fail(e.getErrorCode());
     }
 
+    @ExceptionHandler(BaseException.class)
+    public BaseResponse handlerBaseException(BaseException e) {
+        log.error("BaseException occurred: errorCode={}, message={}", e.getErrorCode(), e.getMessage(), e);
+        return BaseResponse.fail(e.getErrorCode());
+    }
+
     @ExceptionHandler(Exception.class)
     public BaseResponse handlerException(Exception e) {
+        log.error("Unhandled exception: message={}", e.getMessage(), e);
         return BaseResponse.fail(ErrorCode.INTERNAL_SERVER_ERROR);
     }
 }
