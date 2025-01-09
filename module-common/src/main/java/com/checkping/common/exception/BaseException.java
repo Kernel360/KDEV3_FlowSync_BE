4adb77cbca2a063256015b023a6e0b25b74f10c8
oowtl <oowtl7425@gmail.com>
2025-01-09 11:56:49 +0900

    feat : BaseException 생성
    - CustomException 대신 BaseException 이 알맞은 의미라고 판단되어 변경

---
+++ module-common/src/main/java/com/checkping/common/exception/BaseException.java
@@ -0,0 +1,24 @@
+package com.checkping.common.exception;
+
+import com.checkping.common.enums.ErrorCode;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@NoArgsConstructor
+public class BaseException extends RuntimeException {
+
+  private ErrorCode errorCode;
+
+  public BaseException(ErrorCode errorCode) {
+    super(errorCode.getMessage());
+    this.errorCode = errorCode;
+  }
+
+  public BaseException(String message, ErrorCode errorCode) {
+    super(message);
+    this.errorCode = errorCode;
+  }
+}
