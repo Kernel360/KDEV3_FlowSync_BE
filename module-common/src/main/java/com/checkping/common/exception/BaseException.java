dddf27a29ee1b60a0b675ab3b247e697d0aded34
oowtl <oowtl7425@gmail.com>
2025-01-10 14:19:59 +0900

    feat : BaseException 생성
    - BaseException 생성
    - CustomException 은 @Deprecated 로 변경

--- module-common/src/main/java/com/checkping/common/exception/BaseException.java
+++ module-common/src/main/java/com/checkping/common/exception/BaseException.java
@@ -1,20 +1,24 @@
 package com.checkping.common.exception;
 
 import com.checkping.common.enums.ErrorCode;
-import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
 
-@Deprecated
 @Getter
 @Setter
 @NoArgsConstructor
-public class CustomException extends RuntimeException{
+public class BaseException extends RuntimeException {
+
     private ErrorCode errorCode;
 
-    public CustomException(ErrorCode errorCode) {
+    public BaseException(ErrorCode errorCode) {
         super(errorCode.getMessage());
-        setErrorCode(errorCode);
+        this.errorCode = errorCode;
+    }
+
+    public BaseException(String message, ErrorCode errorCode) {
+        super(message);
+        this.errorCode = errorCode;
     }
 }
