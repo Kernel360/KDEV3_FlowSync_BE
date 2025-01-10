3eeef83bdb07e165fb26e9f0053104cb51ffe60f
jaseongkim <jaseong4281@gmail.com>
2025-01-10 15:20:21 +0900

    feat: 조회 기능 서비스, 컨트롤러 구현
    ID를 통한 조회 기능 서비스, API 추가
    타입별 조회 기능 서비스, API 추가
    전체 조회 서비스, API 추가
    서비스 테스트 코드 구현
    컨트롤러 테스트 코드 미구현 상태(구현 예정)
    
    Issue #91

--- module-service/src/main/java/com/checkping/service/member/OrganizationService.java
+++ module-service/src/main/java/com/checkping/service/member/OrganizationService.java
@@ -1,9 +1,19 @@
 package com.checkping.service.member;
 
 import com.checkping.dto.OrganizationRequest;
+import com.checkping.dto.OrganizationResponse;
+
+import java.util.List;
+import java.util.UUID;
 
 public interface OrganizationService {
 
-    void createOrganization(OrganizationRequest.OrganizationSignUpRequest request);
+    OrganizationResponse.ReadResponse createOrganization(OrganizationRequest.CreateRequest request);
+
+    OrganizationResponse.ReadResponse getOrganization(UUID id);
+
+    List<OrganizationResponse.ReadResponse> getByTypeAndStatusOrganizations(String type, String status);
+
+    List<OrganizationResponse.ReadResponse> getAllOrganizations(String status);
 
 }
