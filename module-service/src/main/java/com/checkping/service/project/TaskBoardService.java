34d1f64b2e484ddccb25ebefa4a7490ba53bd8f9
oowtl <oowtl7425@gmail.com>
2025-01-11 00:41:32 +0900

    feat : TaskBoardService.getTaskBoardList
    - searchCondition 을 받아서 TaskBoardReader 에 보내주는 역할
    
    관련 이슈
    - #51

--- module-service/src/main/java/com/checkping/service/project/TaskBoardService.java
+++ module-service/src/main/java/com/checkping/service/project/TaskBoardService.java
@@ -2,7 +2,10 @@ package com.checkping.service.project;
 
 import com.checkping.dto.TaskBoardRequest;
 import com.checkping.dto.TaskBoardResponse;
+import com.checkping.dto.TaskBoardResponse.TaskBoardDto;
+import java.util.List;
 
 public interface TaskBoardService {
     TaskBoardResponse.TaskBoardDto register(TaskBoardRequest.RegisterDto request);
+    List<TaskBoardDto> getTaskBoardList(TaskBoardRequest.SearchCondition searchCondition);
 }
