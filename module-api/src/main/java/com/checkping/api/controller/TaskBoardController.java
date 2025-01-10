2980bca909123c18320b5b1265ecb351a84c0e05
oowtl <oowtl7425@gmail.com>
2025-01-11 00:42:43 +0900

    feat : TaskBoardController
    - RequestParam 을 String 으로 받음
    - SearchCondition 을 통해서 문자열을 Enum 으로 변환 및 예외 처리
    
    관련 이슈
    - #51

--- module-api/src/main/java/com/checkping/api/controller/TaskBoardController.java
+++ module-api/src/main/java/com/checkping/api/controller/TaskBoardController.java
@@ -2,12 +2,17 @@ package com.checkping.api.controller;
 
 import com.checkping.common.response.BaseResponse;
 import com.checkping.dto.TaskBoardRequest;
+import com.checkping.dto.TaskBoardRequest.SearchCondition;
 import com.checkping.dto.TaskBoardResponse;
+import com.checkping.dto.TaskBoardResponse.TaskBoardDto;
 import com.checkping.service.project.TaskBoardService;
+import java.util.List;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @Slf4j
@@ -18,10 +23,26 @@ public class TaskBoardController {
     private final TaskBoardService taskBoardService;
 
     @PostMapping("/posts")
-    public BaseResponse<TaskBoardResponse.TaskBoardDto> register(@RequestBody TaskBoardRequest.RegisterDto request) {
+    public BaseResponse<TaskBoardResponse.TaskBoardDto> register(
+        @RequestBody TaskBoardRequest.RegisterDto request) {
 
         TaskBoardResponse.TaskBoardDto taskBoardDto = taskBoardService.register(request);
 
         return BaseResponse.success(taskBoardDto);
+    }
+
+    @GetMapping("/posts")
+    public BaseResponse<List<TaskBoardDto>> getTaskBoardList(
+        @RequestParam(required = false) String boardCategory,
+        @RequestParam(required = false) String boardStatus) {
+
+        // RequestParam -> SearchCondition
+        TaskBoardRequest.SearchCondition searchCondition = new SearchCondition(boardCategory,
+            boardStatus);
+
+        // getTaskBoardList
+        List<TaskBoardDto> taskBoardDtoList = taskBoardService.getTaskBoardList(searchCondition);
+
+        return BaseResponse.success(taskBoardDtoList);
     }
 }
