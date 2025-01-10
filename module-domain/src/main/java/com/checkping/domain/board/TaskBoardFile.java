6abf92c5e2628dab44357ba0b3617d56d088af8b
KJU3 <kju_3@naver.com>
2025-01-10 19:05:32 +0900

    feat: TaskBoardFile Entity 구현
    TaskBoardFile 엔티티 구현

---
+++ module-domain/src/main/java/com/checkping/domain/board/TaskBoardFile.java
@@ -0,0 +1,43 @@
+package com.checkping.domain.board;
+
+import jakarta.persistence.*;
+import lombok.*;
+
+@Getter
+@Setter
+@Builder
+@ToString
+@AllArgsConstructor
+@NoArgsConstructor
+@Table(name = "task_board_file")
+@Entity
+public class TaskBoardFile extends BaseEntity {
+    /*
+    id : id
+    taskBoardId : 게시글 번호
+    replyList : 답글 리스트
+    originalName : 원본 파일명
+    saveName : 저장 파일명
+    url : 저장 URL
+    size : 파일 Size
+     */
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(name = "task_board_id")
+    private Long taskBoardId;
+
+    @Column(name = "original_name")
+    private String originalName;
+
+    @Column(name = "save_name")
+    private String saveName;
+
+    @Column(name = "url")
+    private String url;
+
+    @Column(name = "size")
+    private long size;
+}
