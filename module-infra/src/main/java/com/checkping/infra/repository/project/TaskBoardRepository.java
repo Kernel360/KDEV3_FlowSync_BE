23f714ab3f683165890563bd17aef32e2ff91268
oowtl <oowtl7425@gmail.com>
2025-01-11 00:32:09 +0900

    feat : TaskBoardRepository find 추가
    - findByBoardCategory 추가
    - findByBoardStatus 추가
    - findByBoardCategoryAndBoardStatus 추가
    
    관련 이슈
    - #51

--- module-infra/src/main/java/com/checkping/infra/repository/project/TaskBoardRepository.java
+++ module-infra/src/main/java/com/checkping/infra/repository/project/TaskBoardRepository.java
@@ -1,10 +1,19 @@
 package com.checkping.infra.repository.project;
 
 import com.checkping.domain.board.TaskBoard;
+import com.checkping.domain.board.TaskBoard.BoardCategory;
+import com.checkping.domain.board.TaskBoard.BoardStatus;
+import java.util.List;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
 public interface TaskBoardRepository extends JpaRepository<TaskBoard, Long> {
 
+    List<TaskBoard> findByBoardCategory(BoardCategory boardCategory);
+
+    List<TaskBoard> findByBoardStatus(BoardStatus boardStatus);
+
+    List<TaskBoard> findByBoardCategoryAndBoardStatus(BoardCategory boardCategory,
+        BoardStatus boardStatus);
 }
