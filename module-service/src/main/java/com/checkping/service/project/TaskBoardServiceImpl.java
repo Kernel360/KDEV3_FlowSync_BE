1578d4aaaaacddee5e993f3fb4784be34394036b
oowtl <oowtl7425@gmail.com>
2025-01-11 00:41:32 +0900

    feat : TaskBoardService.getTaskBoardList
    - searchCondition 을 받아서 TaskBoardReader 에 보내주는 역할
    
    관련 이슈
    - #51

--- module-service/src/main/java/com/checkping/service/project/TaskBoardServiceImpl.java
+++ module-service/src/main/java/com/checkping/service/project/TaskBoardServiceImpl.java
@@ -2,8 +2,12 @@ package com.checkping.service.project;
 
 import com.checkping.domain.board.TaskBoard;
 import com.checkping.dto.TaskBoardRequest;
+import com.checkping.dto.TaskBoardRequest.SearchCondition;
 import com.checkping.dto.TaskBoardResponse;
+import com.checkping.dto.TaskBoardResponse.TaskBoardDto;
+import com.checkping.infra.repository.project.TaskBoardReader;
 import com.checkping.infra.repository.project.TaskBoardStore;
+import java.util.List;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
@@ -12,6 +16,7 @@ import org.springframework.stereotype.Service;
 public class TaskBoardServiceImpl implements TaskBoardService {
 
     private final TaskBoardStore taskBoardStore;
+    private final TaskBoardReader taskBoardReader;
 
     /**
      * 업무 관리 게시글 등록하기
@@ -31,5 +36,23 @@ public class TaskBoardServiceImpl implements TaskBoardService {
 
         // entity -> dto
         return TaskBoardResponse.TaskBoardDto.toDto(taskBoard);
+    }
+
+    /**
+     * TaskBoard 조회 하기 (게시글 유형, 게시글 상태 별 필터링)
+     *
+     * @param searchCondition RequestParam 에서 받아오는 String 을 관리하는 타입
+     * @return 조회한 TaskBoardDto 의 리스트
+     */
+    @Override
+    public List<TaskBoardDto> getTaskBoardList(SearchCondition searchCondition) {
+
+        // 조회
+        List<TaskBoard> taskBoardList = taskBoardReader.getTaskBoard(
+            searchCondition.getBoardCategory(),
+            searchCondition.getBoardStatus());
+
+        // TaskBoard -> TaskBoardDto
+        return taskBoardList.stream().map(TaskBoardDto::toDto).toList();
     }
 }
