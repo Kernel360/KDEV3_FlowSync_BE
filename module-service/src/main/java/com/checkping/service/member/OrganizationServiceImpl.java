7be36e756734f64ce7b9dee5f26899d9174247d0
jaseongkim <jaseong4281@gmail.com>
2025-01-10 15:20:21 +0900

    feat: 조회 기능 서비스, 컨트롤러 구현
    ID를 통한 조회 기능 서비스, API 추가
    타입별 조회 기능 서비스, API 추가
    전체 조회 서비스, API 추가
    서비스 테스트 코드 구현
    컨트롤러 테스트 코드 미구현 상태(구현 예정)
    
    Issue #91

--- module-service/src/main/java/com/checkping/service/member/OrganizationServiceImpl.java
+++ module-service/src/main/java/com/checkping/service/member/OrganizationServiceImpl.java
@@ -4,9 +4,15 @@ import com.checkping.common.enums.ErrorCode;
 import com.checkping.common.exception.CustomException;
 import com.checkping.domain.member.Organization;
 import com.checkping.dto.OrganizationRequest;
+import com.checkping.dto.OrganizationResponse;
 import com.checkping.infra.repository.member.OrganizationRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.Optional;
+import java.util.UUID;
+import java.util.stream.Collectors;
 
 @Service
 @RequiredArgsConstructor
@@ -15,7 +21,7 @@ public class OrganizationServiceImpl implements OrganizationService {
     private final OrganizationRepository organizationRepository;
 
     @Override
-    public void createOrganization(OrganizationRequest.OrganizationSignUpRequest request) {
+    public OrganizationResponse.ReadResponse createOrganization(OrganizationRequest.CreateRequest request) {
 
         if (request == null) {
             throw new CustomException(ErrorCode.BAD_REQUEST);
@@ -25,8 +31,46 @@ public class OrganizationServiceImpl implements OrganizationService {
             throw new CustomException(ErrorCode.BAD_REQUEST);
         }
 
-        Organization organization = OrganizationRequest.OrganizationSignUpRequest.toEntity(request);
+        Organization organization = OrganizationRequest.CreateRequest.toEntity(request);
 
-        organizationRepository.save(organization);
+        Organization createOrganization = organizationRepository.save(organization);
+
+        return OrganizationResponse.ReadResponse.toDto(createOrganization);
+    }
+
+    @Override
+    public OrganizationResponse.ReadResponse getOrganization(UUID id) {
+
+        Optional<Organization> result = organizationRepository.findById(id);
+
+        Organization organization = result.orElseThrow(() -> new CustomException(ErrorCode.NOT_FOUND));
+
+        return OrganizationResponse.ReadResponse.toDto(organization);
+    }
+
+    @Override
+    public List<OrganizationResponse.ReadResponse> getByTypeAndStatusOrganizations(String type, String status) {
+        if (status == null) {
+            return organizationRepository.findByType(Organization.Type.valueOf(type.toUpperCase())).stream()
+                    .map(OrganizationResponse.ReadResponse::toDto)
+                    .collect(Collectors.toList());
+        }
+        return organizationRepository.findByTypeAndStatus(
+                        Organization.Type.valueOf(type.toUpperCase()),
+                        Organization.Status.valueOf(status.toUpperCase())).stream()
+                .map(OrganizationResponse.ReadResponse::toDto)
+                .collect(Collectors.toList());
+    }
+
+    @Override
+    public List<OrganizationResponse.ReadResponse> getAllOrganizations(String status) {
+        if (status == null) {
+            return organizationRepository.findAll().stream()
+                    .map(OrganizationResponse.ReadResponse::toDto)
+                    .collect(Collectors.toList());
+        }
+        return organizationRepository.findByStatus(Organization.Status.valueOf(status.toUpperCase())).stream()
+                .map(OrganizationResponse.ReadResponse::toDto)
+                .collect(Collectors.toList());
     }
 }
--- module-service/src/test/java/com/checkping/service/OrganizationServiceTests.java
+++ module-service/src/test/java/com/checkping/service/OrganizationServiceTests.java
@@ -1,10 +1,16 @@
 package com.checkping.service;
 
+import com.checkping.domain.member.Organization;
 import com.checkping.dto.OrganizationRequest;
+import com.checkping.dto.OrganizationResponse;
+import com.checkping.infra.repository.member.OrganizationRepository;
 import com.checkping.service.member.OrganizationService;
+import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+
+import java.util.List;
 
 @SpringBootTest
 class OrganizationServiceTests {
@@ -12,18 +18,67 @@ class OrganizationServiceTests {
     @Autowired
     private OrganizationService organizationService;
 
+    @Autowired
+    private OrganizationRepository organizationRepository;
+
+    @BeforeAll
+    static void init(@Autowired OrganizationService organizationService) {
+        for (int i = 0; i < 10; i++) {
+            organizationService.createOrganization(OrganizationRequest.CreateRequest.builder()
+                    .type(i < 5 ? "CUSTOMER" : "DEVELOPER")
+                    .brNumber("123456" + i)
+                    .name("커널" + i)
+                    .brCertificateUrl("cert-url-")
+                    .streetAddress("강남대로")
+                    .detailAddress("")
+                    .phoneNumber("")
+                    .build());
+        }
+    }
+
     @Test
     void testCreateOrganization() {
-
-        organizationService.createOrganization(OrganizationRequest.OrganizationSignUpRequest.builder()
+        organizationService.createOrganization(OrganizationRequest.CreateRequest.builder()
                 .type("CUSTOMER")
-                .brNumber("123456")
-                .name("커널고객사")
+                .brNumber("123456mnb")
+                .name("새로운 커널고객사")
                 .brCertificateUrl("awge76876w-asdg7djh")
                 .streetAddress("강남대로 364")
                 .detailAddress("")
                 .phoneNumber("")
                 .build());
+    }
+
+    @Test
+    void testGetOrganizationById() {
+        Organization organization =
+                organizationRepository.save(OrganizationRequest.CreateRequest.toEntity(OrganizationRequest.CreateRequest.builder()
+                        .type("CUSTOMER")
+                        .brNumber("123456mnb")
+                        .name("새로운 커널")
+                        .brCertificateUrl("awge76876w-asdg7djh")
+                        .streetAddress("강남대로 364")
+                        .detailAddress("")
+                        .phoneNumber("")
+                        .build()));
+
+        OrganizationResponse.ReadResponse organizationResponse =
+                organizationService.getOrganization(organization.getId());
+    }
+
+    @Test
+    void testGetByTypeOrganization() {
+        List<OrganizationResponse.ReadResponse> organizationCsList =
+                organizationService.getByTypeAndStatusOrganizations("CUSTOMER", null);
+
+        List<OrganizationResponse.ReadResponse> organizationDevList =
+                organizationService.getByTypeAndStatusOrganizations("DEVELOPER", "ACTIVE");
+    }
+
+    @Test
+    void testGetAllOrganizations() {
+        List<OrganizationResponse.ReadResponse> organizations =
+                organizationService.getAllOrganizations("ACTIVE");
     }
 
 }
