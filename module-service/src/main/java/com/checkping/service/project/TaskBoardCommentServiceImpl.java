1a753a5adac3be5ef64843ad00254a7f348465e5
oowtl <oowtl7425@gmail.com>
2025-01-12 02:18:18 +0900

    feat : TaskBoardComment 서비스 - 삭제 기능
    - TaskBoardCommentService.deleteSoft 메서드 추가
    - TaskBoardCommentService.deleteHard 메서드 추가
    
    관련 이슈
    - #60

--- module-service/src/main/java/com/checkping/service/project/TaskBoardCommentServiceImpl.java
+++ module-service/src/main/java/com/checkping/service/project/TaskBoardCommentServiceImpl.java
@@ -5,8 +5,12 @@ import com.checkping.domain.board.TaskBoardComment;
 import com.checkping.dto.TaskBoardCommentRequest;
 import com.checkping.dto.TaskBoardCommentRequest.RegisterDto;
 import com.checkping.dto.TaskBoardCommentResponse;
+import com.checkping.dto.TaskBoardCommentResponse.TaskBoardCommentDto;
+import com.checkping.exception.project.TaskBoardCommentMisMatchEntityException;
+import com.checkping.exception.project.TaskBoardCommentNotFoundEntityException;
 import com.checkping.exception.project.TaskBoardNotFoundEntityException;
 import com.checkping.infra.repository.project.taskboard.TaskBoardReader;
+import com.checkping.infra.repository.project.taskboardcomment.TaskBoardCommentReader;
 import com.checkping.infra.repository.project.taskboardcomment.TaskBoardCommentStore;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
@@ -17,6 +21,7 @@ public class TaskBoardCommentServiceImpl implements TaskBoardCommentService {
 
     private final TaskBoardReader taskBoardReader;
     private final TaskBoardCommentStore taskBoardCommentStore;
+    private final TaskBoardCommentReader taskBoardCommentReader;
 
     /**
      * 업무 관리 게시글 서비스 - 등록 기능
@@ -43,5 +48,64 @@ public class TaskBoardCommentServiceImpl implements TaskBoardCommentService {
 
         // Entity -> Dto
         return TaskBoardCommentResponse.TaskBoardCommentDto.toDto(taskBoardComment);
+    }
+
+    /**
+     * 업무 관리 게시글 댓글 서비스 - 댓글 소프트 삭제
+     *
+     * @param taskBoardId 업무 관리 게시글 ID
+     * @param taskBoardCommentId 업무 관리 게시글 댓글 ID
+     * @return 삭제 상태인 TaskBoardComment
+     */
+    @Override
+    public TaskBoardCommentDto deleteSoft(Long taskBoardId, Long taskBoardCommentId) {
+
+        // 업무 관리 게시글에 속한 댓글인지 확인
+        boolean isContaining = taskBoardCommentReader.checkCommentContaining(taskBoardId, taskBoardCommentId);
+        if (!isContaining) {
+            throw new TaskBoardCommentMisMatchEntityException();
+        }
+
+        // find TaskBoardComment Entity
+        TaskBoardComment initComment = taskBoardCommentReader.getByTaskBoardCommentId(
+            taskBoardCommentId).orElseThrow(
+            TaskBoardCommentNotFoundEntityException::new);
+
+        // soft delete
+        initComment.deactivate();
+
+        // save
+        TaskBoardComment deletedTaskBoardComment = taskBoardCommentStore.store(initComment);
+
+        // Entity -> Dto
+        return TaskBoardCommentResponse.TaskBoardCommentDto.toDto(deletedTaskBoardComment);
+    }
+
+    /**
+     * 업무 관리 게시글 댓글 서비스 - 댓글 하드 삭제
+     *
+     * @param taskBoardId 업무 관리 게시글 ID
+     * @param taskBoardCommentId 업무 관리 게시글 댓글 ID
+     * @return 삭제된 TaskBoardComment
+     */
+    @Override
+    public TaskBoardCommentDto deleteHard(Long taskBoardId, Long taskBoardCommentId) {
+
+        // 업무 관리 게시글에 속한 댓글인지 확인
+        boolean isContaining = taskBoardCommentReader.checkCommentContaining(taskBoardId, taskBoardCommentId);
+        if (!isContaining) {
+            throw new TaskBoardCommentMisMatchEntityException();
+        }
+
+        // find TaskBoardComment Entity
+        TaskBoardComment initComment = taskBoardCommentReader.getByTaskBoardCommentId(
+            taskBoardCommentId).orElseThrow(
+            TaskBoardCommentNotFoundEntityException::new);
+
+        // HARD DELETE
+        taskBoardCommentStore.deleteHard(initComment);
+
+        // Entity -> Dto
+        return TaskBoardCommentResponse.TaskBoardCommentDto.toDto(initComment);
     }
 }
