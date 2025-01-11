578671aec5094bab7994565f005a441b92caa9fd
oowtl <oowtl7425@gmail.com>
2025-01-11 22:33:47 +0900

    feat : TaskBoardComment(Entity) 리스트를 Dto 리스트로 변경하는 기능
    - Entity 가 그대로 반환될 때 순환 참조가 일어나는 경우가 존재함에 따라 Entity를 Dto 로 변환할 기능이 필요
    - commentList 가 null 이면 empty List 반환
    
    관련 이슈
    - #53

--- module-service/src/main/java/com/checkping/dto/TaskBoardCommentResponse.java
+++ module-service/src/main/java/com/checkping/dto/TaskBoardCommentResponse.java
@@ -3,6 +3,8 @@ package com.checkping.dto;
 import com.checkping.domain.board.TaskBoardComment;
 import com.checkping.domain.board.TaskBoardComment.DeleteStatus;
 import java.time.LocalDateTime;
+import java.util.Collections;
+import java.util.List;
 import lombok.AccessLevel;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
@@ -14,6 +16,7 @@ public class TaskBoardCommentResponse {
     @Getter
     @ToString
     public static class TaskBoardCommentDto {
+
         private Long taskBoardCommentId;
         private String content;
         private LocalDateTime regAt;
@@ -27,13 +30,30 @@ public class TaskBoardCommentResponse {
          * @param taskBoardComment TaskBoardComment Entity
          * @return TaskBoardCommentDto
          */
-        public static TaskBoardCommentDto toDto (TaskBoardComment taskBoardComment) {
+        public static TaskBoardCommentDto toDto(TaskBoardComment taskBoardComment) {
             TaskBoardCommentDto dto = new TaskBoardCommentDto();
             dto.taskBoardCommentId = taskBoardComment.getId();
             dto.content = taskBoardComment.getContent();
             dto.regAt = taskBoardComment.getRegAt();
             dto.editAt = taskBoardComment.getEditAt();
             return dto;
+        }
+
+        /**
+         * TaskBoardComment 리스트를 TaskBoardCommentDto 리스트로 변환
+         *
+         * @param taskBoardCommentList TaskBoardComment 의 리스트
+         * @return TaskBoardCommentDto 리스트
+         */
+        public static List<TaskBoardCommentDto> toDtoList(
+            List<TaskBoardComment> taskBoardCommentList) {
+
+            // null check
+            if (taskBoardCommentList == null) {
+                return Collections.emptyList();
+            }
+
+            return taskBoardCommentList.stream().map(TaskBoardCommentDto::toDto).toList();
         }
 
     }
