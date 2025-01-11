e8518ddd8a6ad70cef73b267440cbf0253cf4562
oowtl <oowtl7425@gmail.com>
2025-01-11 15:10:09 +0900

    feat : TaskBoardReader.getTaskBoardById 추가
    - TaskBoardComment 등록을 위한 TaskBoard 조회 기능 추가
    
    관련 이슈
    - #56

--- module-infra/src/main/java/com/checkping/infra/repository/project/taskboard/TaskBoardReader.java
+++ module-infra/src/main/java/com/checkping/infra/repository/project/taskboard/TaskBoardReader.java
@@ -2,7 +2,9 @@ package com.checkping.infra.repository.project.taskboard;
 
 import com.checkping.domain.board.TaskBoard;
 import java.util.List;
+import java.util.Optional;
 
 public interface TaskBoardReader {
     List<TaskBoard> getTaskBoard(TaskBoard.BoardCategory boardCategory, TaskBoard.BoardStatus boardStatus);
+    Optional<TaskBoard> getTaskBoardById(Long id);
 }
