4a069702a0de348dcadd3ff53571d1349c3bfa1c
oowtl <oowtl7425@gmail.com>
2025-01-11 22:35:02 +0900

    feat : TaskBoardResponse Dto 추가 및 변경
    - 목록 조회와 상세 조회를 목적으로 하는 Dto 로 분리
    - 목록 조회 : TaskBoardResponse.TaskBoardListDto
    - 상세 조회 : TaskBoardResponse.TaskBoardItemDto
    - TaskBoardResponse 를 생성하지 못하도록 생성자에 PRIVATE 설정
    
    관련 이슈
    - #53

--- module-service/src/main/java/com/checkping/dto/TaskBoardResponse.java
+++ module-service/src/main/java/com/checkping/dto/TaskBoardResponse.java
@@ -1,17 +1,22 @@
 package com.checkping.dto;
 
 import com.checkping.domain.board.TaskBoard;
+import com.checkping.dto.TaskBoardResponse.TaskBoardListDto;
 import java.time.LocalDateTime;
+import java.util.List;
+import lombok.AccessLevel;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 import lombok.Setter;
 import lombok.ToString;
 
+@NoArgsConstructor(access = AccessLevel.PRIVATE)
 public class TaskBoardResponse {
 
     @Getter
     @Setter
     @ToString
-    public static class TaskBoardDto {
+    public static class TaskBoardListDto {
         /*
         id : 게시글 고유 ID
         number : 게시글 번호
@@ -35,8 +40,8 @@ public class TaskBoardResponse {
         private TaskBoard.BoardStatus boardStatus;
         private TaskBoard.DeleteStatus deletedYn;
 
-        public static TaskBoardDto toDto(TaskBoard taskBoard) {
-            TaskBoardDto boardDto = new TaskBoardDto();
+        public static TaskBoardListDto toDto(TaskBoard taskBoard) {
+            TaskBoardListDto boardDto = new TaskBoardListDto();
             boardDto.setId(taskBoard.getId());
             boardDto.setNumber(taskBoard.getNumber());
             boardDto.setTitle(taskBoard.getTitle());
@@ -46,6 +51,57 @@ public class TaskBoardResponse {
             boardDto.setApproverAt(taskBoard.getApproverAt());
             boardDto.setBoardCategory(taskBoard.getBoardCategory());
             boardDto.setBoardStatus(taskBoard.getBoardStatus());
+            return boardDto;
+        }
+    }
+
+    @Getter
+    @Setter
+    @ToString
+    public static class TaskBoardItemDto {
+
+        /*
+        id : 게시글 고유 ID
+        number : 게시글 번호
+        title : 게시글 제목
+        content : 게시글 본문 내용
+        regAt : 게시글 작성 일시
+        editAt : 게시글 마지막 수정 일시
+        approverAt : 게시글 승인 일시
+        boardCategory : 게시글 카테고리 (enum, String)
+        boardStatus : 게시글 상태 (enum, String)
+        deletedYn : 게시글 삭제 여부 ('Y' 또는 'N')
+        commentList : 게시글 댓글 리스트
+         */
+        private Long id;
+        private Integer number;
+        private String title;
+        private String content;
+        private LocalDateTime regAt;
+        private LocalDateTime editAt;
+        private LocalDateTime approverAt;
+        private TaskBoard.BoardCategory boardCategory;
+        private TaskBoard.BoardStatus boardStatus;
+        private TaskBoard.DeleteStatus deletedYn;
+        private List<TaskBoardCommentResponse.TaskBoardCommentDto> commentList;
+
+        public static TaskBoardResponse.TaskBoardItemDto toDto(TaskBoard taskBoard) {
+            TaskBoardResponse.TaskBoardItemDto boardDto = new TaskBoardResponse.TaskBoardItemDto();
+            boardDto.setId(taskBoard.getId());
+            boardDto.setNumber(taskBoard.getNumber());
+            boardDto.setTitle(taskBoard.getTitle());
+            boardDto.setContent(taskBoard.getContent());
+            boardDto.setRegAt(taskBoard.getRegAt());
+            boardDto.setEditAt(taskBoard.getEditAt());
+            boardDto.setApproverAt(taskBoard.getApproverAt());
+            boardDto.setBoardCategory(taskBoard.getBoardCategory());
+            boardDto.setBoardStatus(taskBoard.getBoardStatus());
+
+            // Entity -> Dto
+            List<TaskBoardCommentResponse.TaskBoardCommentDto> comments =
+                TaskBoardCommentResponse.TaskBoardCommentDto.toDtoList(taskBoard.getCommentList());
+            boardDto.setCommentList(comments);
+
             return boardDto;
         }
     }
