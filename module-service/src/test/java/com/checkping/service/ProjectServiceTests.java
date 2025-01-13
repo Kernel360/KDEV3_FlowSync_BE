935880bfebda9f5163f2bc81604ed35db61b46ff
a121255 <a132258@naver.com>
2025-01-13 19:50:07 +0900

    feat : 프로젝트 조회 테스트 기능
    - ProjectServiceTests
        - 프로젝트 조회 테스트

--- module-service/src/test/java/com/checkping/service/ProjectServiceTests.java
+++ module-service/src/test/java/com/checkping/service/ProjectServiceTests.java
@@ -1,10 +1,13 @@
 package com.checkping.service;
 
+import com.checkping.domain.member.Organization;
 import com.checkping.domain.project.Project;
 import com.checkping.dto.ProjectRequest;
 import com.checkping.dto.ProjectResponse;
+import com.checkping.infra.repository.member.OrganizationRepository;
 import com.checkping.infra.repository.project.ProjectRepository;
 import com.checkping.service.project.ProjectServiceImpl;
+import jakarta.transaction.Transactional;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
@@ -12,6 +15,7 @@ import org.springframework.test.context.ActiveProfiles;
 
 import java.time.LocalDate;
 import java.time.LocalDateTime;
+import java.util.ArrayList;
 import java.util.List;
 
 
@@ -23,6 +27,9 @@ public class ProjectServiceTests {
 
     @Autowired
     private ProjectRepository projectRepository;
+
+    @Autowired
+    private OrganizationRepository organizationRepository;
 
     @Test
     public void resisterProject() {
@@ -96,6 +103,53 @@ public class ProjectServiceTests {
         for (int i = 0; i < list.size(); i++) {
             System.out.println("updateProject : " + list.get(i));
         }
+
+    }
+
+    @Test
+    @Transactional
+    public void findAllProject(){
+
+        // 테스트 업체 추가
+        Organization dev_organization = Organization.builder()
+                .name("DEV_kernel")
+                .type(Organization.Type.DEVELOPER)
+                .status(Organization.Status.ACTIVE)
+                .regAt(LocalDateTime.now())
+                .build();
+        organizationRepository.save(dev_organization);
+
+        Organization cust_organization = Organization.builder()
+                .name("CUST_kernel2")
+                .type(Organization.Type.CUSTOMER)
+                .status(Organization.Status.ACTIVE)
+                .regAt(LocalDateTime.now())
+                .build();
+        organizationRepository.save(cust_organization);
+        List<Organization> organizations = new ArrayList<>();
+
+        // 테스트 프로젝트 추가
+        Project project = Project.builder()
+                .name("이름")
+                .description("설명")
+                .detail("상세설명")
+                .status(Project.Status.IN_PROGRESS)
+                .regAt(LocalDateTime.now())
+                .closeAt(LocalDate.parse("2025-12-30").atStartOfDay())
+                .resisterId(49283L)
+                .organizations(organizations)
+                .build();
+
+        project.getOrganizations().add(dev_organization);
+        project.getOrganizations().add(cust_organization);
+
+        projectRepository.save(project);
+
+        List<Project> projects = projectRepository.findAll();
+        System.out.println("p : " + projects.get(0));
+
+        List<ProjectResponse.ProjectDto> list = projectService.findAllProjects("이름","IN_PROGRESS");
+        System.out.println(list);
 
     }
 }
