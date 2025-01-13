aa96a31f51b6b268bb4b34e5982cc2967aa9e52c
KJU3 <kju_3@naver.com>
2025-01-13 15:48:49 +0900

    feat: CORS 설정 Config 추가

---
+++ module-common/src/main/java/com/checkping/common/config/WebCorsConfig.java
@@ -0,0 +1,19 @@
+package com.checkping.common.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class WebCorsConfig implements WebMvcConfigurer {
+
+    @Override
+    public void addCorsMappings(CorsRegistry registry){
+        registry.addMapping("/**")
+                .allowedOrigins("http://localhost:8080", "http://15.164.86.69:8080", "http://15.164.86.69")
+                .allowedMethods("GET", "POST", "PUT", "DELETE")
+                .allowedHeaders("*")
+                .allowCredentials(true)
+                .maxAge(3600);
+    }
+}
