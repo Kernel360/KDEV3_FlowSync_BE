20159e2806f2ae606a224fe64f9da026e8fc26c1
a121255 <a132258@naver.com>
2025-01-13 19:39:03 +0900

    feat : 프로젝트 조회 Controller, Service 추가
    - ProjectController, ProjectService
        - 관리자 입장에서 전체 프로젝트를 조회
        - 추후 개발사/고객사의 프로젝트 조회 기능 추가 필요
        - 프로젝트 정보와 해당 프로젝트의 개발사, 고객사의 정보 가져옴 (type, name)

--- module-service/src/main/java/com/checkping/service/project/ProjectService.java
+++ module-service/src/main/java/com/checkping/service/project/ProjectService.java
@@ -3,10 +3,14 @@ package com.checkping.service.project;
 import com.checkping.dto.ProjectRequest;
 import com.checkping.dto.ProjectResponse;
 
+import java.util.List;
+
 public interface ProjectService {
     ProjectResponse.ProjectDto registerProject(ProjectRequest.ResisterDto request);
 
     ProjectResponse.ProjectDto deleteProject(Long projectId);
 
     ProjectResponse.ProjectDto updateProject(Long projectId, ProjectRequest.UpdateDto request);
+
+    List<ProjectResponse.ProjectDto> findAllProjects(String keyword, String status);
 }
