5655e1446baa1b158f3de06adf9bcb09c0dfd672
KJU3 <kju_3@naver.com>
2025-01-13 15:40:45 +0900

    feature: Main 클래스 Security 자동 설정 제거

--- module-api/src/main/java/com/checkping/api/ApiApplication.java
+++ module-api/src/main/java/com/checkping/api/ApiApplication.java
@@ -3,6 +3,7 @@ package com.checkping.api;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.domain.EntityScan;
+import org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
@@ -14,6 +15,7 @@ import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
                 "com.checkping.infra",
                 "com.checkping.domain",
         }
+        , exclude = {SecurityAutoConfiguration.class}
 )
 @EntityScan("com.checkping.domain")
 @EnableJpaRepositories(basePackages = "com.checkping.infra.repository")
