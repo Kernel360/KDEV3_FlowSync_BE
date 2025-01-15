6a42706b734345a9b3fc3fbd6f3a730ee2e563b6
oowtl <oowtl7425@gmail.com>
2025-01-16 08:47:51 +0900

    feat : TaskBoardUpdate 생성에 따른 TaskBoardRequest.UpdateDto 삭제
    - TaskBoardRequest.UpdateDto 삭제
    
    관련 이슈
    - #163

--- module-service/src/main/java/com/checkping/dto/TaskBoardRequest.java
+++ module-service/src/main/java/com/checkping/dto/TaskBoardRequest.java
@@ -41,25 +41,6 @@ public class TaskBoardRequest {
         }
     }
 
-    @Getter
-    @ToString
-    public static class UpdateDto {
-        /*
-        title : 게시글 제목
-        content : 게시글 본문
-        boardCategory : 게시글 유형
-        boardStatus : 게시글 상태
-         */
-        @Schema(description = "게시글 제목", example = "게시글 제목 입니다.")
-        private String title;
-        @Schema(description = "게시글 본문", example = "게시글 본문 입니다.")
-        private String content;
-        @Schema(description = "게시글 유형")
-        private String boardCategory;
-        @Schema(description = "게시글 상태")
-        private String boardStatus;
-    }
-
     /**
      * Enum : BoardCategory 변환 함수
      *
