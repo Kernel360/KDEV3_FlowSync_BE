c2026e5219722a068a0726cf614cee7d1154a708
oowtl <oowtl7425@gmail.com>
2025-01-15 13:32:44 +0900

    feat : FileResponse toDto, toDtoList
    - TaskBoardFile Entity 를 Dto 로 변경하는 메서드
    
    관련 이슈
    - #117

--- module-common/src/main/java/com/checkping/common/utils/FileResponse.java
+++ module-common/src/main/java/com/checkping/common/utils/FileResponse.java
@@ -1,5 +1,8 @@
 package com.checkping.common.utils;
 
+import com.checkping.domain.board.TaskBoardFile;
+import java.util.Collections;
+import java.util.List;
 import lombok.Builder;
 
 /**
@@ -9,7 +12,28 @@ import lombok.Builder;
  * @param size         파일 크기
  */
 public record FileResponse(String originalName, String saveName, String url, long size) {
+
     @Builder
     public FileResponse {
+    }
+
+    /**
+     * TaskBoardFile Entity -> FileResponse Dto
+     *
+     * @param taskBoardFile TaskBoardFile Entity
+     * @return FileResponse
+     */
+    public static FileResponse toDto(TaskBoardFile taskBoardFile) {
+        return FileResponse.builder().originalName(taskBoardFile.getOriginalName())
+            .saveName(taskBoardFile.getSaveName()).url(taskBoardFile.getUrl())
+            .size(taskBoardFile.getSize()).build();
+    }
+
+    public static List<FileResponse> toDtoList(List<TaskBoardFile> taskBoardFiles) {
+        if (taskBoardFiles == null || taskBoardFiles.isEmpty()) {
+            return Collections.emptyList();
+        }
+
+        return taskBoardFiles.stream().map(FileResponse::toDto).toList();
     }
 }
