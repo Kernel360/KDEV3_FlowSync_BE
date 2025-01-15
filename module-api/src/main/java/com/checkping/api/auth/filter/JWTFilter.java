f1a826f3f61676728e17c5e4a0bf07a2e884925e
quickestfunctioning <snc01004@naver.com>
2025-01-15 13:55:41 +0900

    chore : JWTFilter 수정
    chore : doFilterInternal 내부에 authorizationHeader == null인 경우와 accessToken == null인 경우에 return하는 if문 삭제

--- module-api/src/main/java/com/checkping/api/auth/filter/JWTFilter.java
+++ module-api/src/main/java/com/checkping/api/auth/filter/JWTFilter.java
@@ -73,22 +73,8 @@ public class JWTFilter extends OncePerRequestFilter {
         // 헤더에서 access키에 담긴 토큰을 꺼내서 Bearer를 제거 후 accessToken에 담음
         String authorizationHeader = request.getHeader("Authorization");
 
-        if (authorizationHeader == null) {
-
-            filterChain.doFilter(request, response); //
-
-            return;
-        }
         String accessToken = authorizationHeader.substring(7);
 
-
-        //토큰이 없다면 다음 필터로 넘김
-        if (accessToken == null) {
-
-            filterChain.doFilter(request, response); //
-
-            return;
-        }
 
         // 토큰 만료 여부 확인, 만료시 다음 필터로 넘기지 않음
         try {
