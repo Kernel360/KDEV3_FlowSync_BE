714265f7f2369402bfc042a399b1f70008d27091
oowtl <oowtl7425@gmail.com>
2025-01-15 13:41:21 +0900

    feat : TaskBoardFileService.saveFiles 기능
    - TaskBoardFileService.saveFiles 기능
    
    관련 이슈
    - #117

--- module-service/src/main/java/com/checkping/service/project/TaskBoardFileService.java
+++ module-service/src/main/java/com/checkping/service/project/TaskBoardFileService.java
@@ -1,9 +1,10 @@
 package com.checkping.service.project;
 
+import com.checkping.common.utils.FileResponse;
 import org.springframework.web.multipart.MultipartFile;
 
 import java.util.List;
 
 public interface TaskBoardFileService {
-    void saveFiles(Long taskBoardId, List<MultipartFile> fileRequests);
+    List<FileResponse> saveFiles(Long taskBoardId, List<MultipartFile> fileRequests);
 }
--- module-service/src/main/java/com/checkping/service/project/TaskBoardFileServiceImpl.java
+++ module-service/src/main/java/com/checkping/service/project/TaskBoardFileServiceImpl.java
@@ -1,41 +1,31 @@
 package com.checkping.service.project;
 
-import com.checkping.common.utils.FileRequest;
-import com.checkping.common.utils.FileUtils;
+import com.checkping.common.utils.FileResponse;
+import com.checkping.domain.board.TaskBoard;
 import com.checkping.domain.board.TaskBoardFile;
-import com.checkping.infra.repository.project.TaskBoardFileRepository;
-import org.springframework.beans.factory.annotation.Qualifier;
+import com.checkping.exception.project.TaskBoardNotFoundEntityException;
+import com.checkping.infra.repository.project.taskboard.TaskBoardReader;
+import com.checkping.infra.repository.project.taskboardfile.TaskBoardFileStore;
+import java.util.List;
+import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 import org.springframework.web.multipart.MultipartFile;
 
-import java.util.List;
-
 @Service
+@RequiredArgsConstructor
 public class TaskBoardFileServiceImpl implements TaskBoardFileService {
-    private final TaskBoardFileRepository fileRepository;
-    private final FileUtils fileUtils;
 
-    public TaskBoardFileServiceImpl(TaskBoardFileRepository fileRepository, @Qualifier("fileUtilsS3") FileUtils fileUtils) {
-        this.fileRepository = fileRepository;
-        this.fileUtils = fileUtils;
-    }
+    private final TaskBoardReader taskBoardReader;
+    private final TaskBoardFileStore taskBoardFileStore;
 
     @Override
-    public void saveFiles(Long taskBoardId, List<MultipartFile> fileRequests) {
-        List<FileRequest> uploadedFiles = fileUtils.uploadFiles(fileRequests);
-        List<TaskBoardFile> files = uploadedFiles.stream()
-                .map(request -> createTaskBoardFile(taskBoardId, request))
-                .toList();
-        fileRepository.saveAll(files);
-    }
+    public List<FileResponse> saveFiles(Long taskBoardId, List<MultipartFile> fileRequests) {
 
-    private TaskBoardFile createTaskBoardFile(Long taskBoardId, FileRequest request) {
-        return TaskBoardFile.builder()
-                .originalName(request.originalName())
-                .saveName(request.saveName())
-                .url(request.url())
-                .size(request.size())
-                .taskBoardId(taskBoardId)
-                .build();
+        TaskBoard taskBoard = taskBoardReader.getTaskBoardById(taskBoardId).orElseThrow(
+            TaskBoardNotFoundEntityException::new);
+
+        List<TaskBoardFile> files = taskBoardFileStore.saveFileList(taskBoard, fileRequests);
+
+        return FileResponse.toDtoList(files);
     }
 }
