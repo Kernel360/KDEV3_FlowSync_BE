54a6d9d47e2c2397b123332a7bc1528e96aac851
quickestfunctioning <snc01004@naver.com>
2025-01-16 20:29:48 +0900

    주석제거

--- module-api/src/main/java/com/checkping/api/controller/ReissueController.java
+++ module-api/src/main/java/com/checkping/api/controller/ReissueController.java
@@ -42,11 +42,4 @@ public class ReissueController {
 
         return BaseResponse.success("Reissue success");
     }
-
-//    private Cookie createCookie(String key, String value) {
-//        Cookie cookie = new Cookie(key, value);
-//        cookie.setMaxAge(24 * 60 * 60);
-//        cookie.setHttpOnly(true);
-//        return cookie;
-//    }
 }
