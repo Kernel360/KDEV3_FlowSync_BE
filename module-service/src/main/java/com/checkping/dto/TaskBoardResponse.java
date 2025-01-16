6cfe0183a1f6fc14278da37de08d42ec9a653151
oowtl <oowtl7425@gmail.com>
2025-01-16 09:35:28 +0900

    feat : TaskBoardGetList 생성에 따른 TaskBoardListDto 삭제
    - Dto 클래스 삭제
    
    관련 이슈
    - #163

--- module-service/src/main/java/com/checkping/dto/TaskBoardResponse.java
+++ module-service/src/main/java/com/checkping/dto/TaskBoardResponse.java
@@ -2,10 +2,8 @@ package com.checkping.dto;
 
 import com.checkping.common.utils.FileResponse;
 import com.checkping.domain.board.TaskBoard;
-import com.checkping.dto.TaskBoardResponse.TaskBoardListDto;
 import com.fasterxml.jackson.annotation.JsonFormat;
 import io.swagger.v3.oas.annotations.media.Schema;
-import jakarta.persistence.Column;
 import java.time.LocalDateTime;
 import java.util.List;
 import lombok.AccessLevel;
@@ -17,60 +15,6 @@ import lombok.ToString;
 @NoArgsConstructor(access = AccessLevel.PRIVATE)
 public class TaskBoardResponse {
 
-    @Getter
-    @Setter
-    @ToString
-    public static class TaskBoardListDto {
-
-        /*
-        id : 게시글 고유 ID
-        number : 게시글 번호
-        title : 게시글 제목
-        content : 게시글 본문 내용
-        regAt : 게시글 작성 일시
-        editAt : 게시글 마지막 수정 일시
-        approverAt : 게시글 승인 일시
-        boardCategory : 게시글 카테고리 (enum, String)
-        boardStatus : 게시글 상태 (enum, String)
-        deletedYn : 게시글 삭제 여부 ('Y' 또는 'N')
-         */
-        private Long id;
-        @Schema(description = "게시글 번호")
-        private Integer number;
-        @Schema(description = "게시글 제목", example = "게시글 제목 입니다.")
-        private String title;
-        @Schema(description = "게시글 본문", example = "게시글 본문 입니다.")
-        private String content;
-        @Schema(description = "등록 일시")
-        @JsonFormat(pattern = "yyyy-MM-dd HH:mm:ss")
-        private LocalDateTime regAt;
-        @Schema(description = "수정 일시")
-        @JsonFormat(pattern = "yyyy-MM-dd HH:mm:ss")
-        private LocalDateTime editAt;
-        @Schema(description = "승인 일시")
-        @JsonFormat(pattern = "yyyy-MM-dd HH:mm:ss")
-        private LocalDateTime approverAt;
-        @Schema(description = "게시글 유형")
-        private TaskBoard.BoardCategory boardCategory;
-        @Schema(description = "게시글 상태")
-        private TaskBoard.BoardStatus boardStatus;
-        private TaskBoard.DeleteStatus deletedYn;
-
-        public static TaskBoardListDto toDto(TaskBoard taskBoard) {
-            TaskBoardListDto boardDto = new TaskBoardListDto();
-            boardDto.setId(taskBoard.getId());
-            boardDto.setNumber(taskBoard.getNumber());
-            boardDto.setTitle(taskBoard.getTitle());
-            boardDto.setContent(taskBoard.getContent());
-            boardDto.setRegAt(taskBoard.getRegAt());
-            boardDto.setEditAt(taskBoard.getEditAt());
-            boardDto.setApproverAt(taskBoard.getApproverAt());
-            boardDto.setBoardCategory(taskBoard.getBoardCategory());
-            boardDto.setBoardStatus(taskBoard.getBoardStatus());
-            boardDto.setDeletedYn(taskBoard.getDeletedYn());
-            return boardDto;
-        }
-    }
 
     @Getter
     @Setter
