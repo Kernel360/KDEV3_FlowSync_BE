92ffd515b246afaf5ecbbe4cfd6ac9159e0427e2
oowtl <oowtl7425@gmail.com>
2025-01-20 22:39:03 +0900

    refactor : module-service - QuestionFileService 로 변경
    - TaskBoardFileService -> QuestionFileService
    
    연관 이슈
    - #202

--- module-service/src/main/java/com/checkping/service/question/file/QuestionFileService.java
+++ module-service/src/main/java/com/checkping/service/question/file/QuestionFileService.java
@@ -5,6 +5,6 @@ import org.springframework.web.multipart.MultipartFile;
 
 import java.util.List;
 
-public interface TaskBoardFileService {
+public interface QuestionFileService {
     List<FileResponse> saveFiles(Long taskBoardId, List<MultipartFile> fileRequests);
 }
