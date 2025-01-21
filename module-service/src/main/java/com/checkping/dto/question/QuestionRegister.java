a62c4c3bb04da231750611e427495e46d3abfc6f
oowtl <oowtl7425@gmail.com>
2025-01-21 22:16:29 +0900

    feat : QuestionRegister.Request 수정
    - status, category 속성 설명 제거
    - fileInfoList request 추가
    
    연관 이슈
    - #210

--- module-service/src/main/java/com/checkping/dto/question/QuestionRegister.java
+++ module-service/src/main/java/com/checkping/dto/question/QuestionRegister.java
@@ -1,23 +1,27 @@
 package com.checkping.dto.question;
 
+import com.checkping.common.utils.FileRequest;
 import com.checkping.domain.question.Question;
 import com.checkping.dto.question.link.QuestionLinkRequest.RegisterDto;
 import io.swagger.v3.oas.annotations.media.Schema;
 import java.util.List;
 import lombok.AccessLevel;
+import lombok.Getter;
 import lombok.NoArgsConstructor;
+import lombok.ToString;
 
 @NoArgsConstructor(access = AccessLevel.PRIVATE)
 public class QuestionRegister {
 
+    @Getter
+    @ToString
     public static class Request {
 
         /*
         title : 게시글 제목
         content : 게시글 본문 내용
-        category : 게시글 카테고리 (enum, String
-        status : 게시글 상태 (enum, String)
         linkList : 첨부 링크 리스트 (List<QuestionLinkRequest.RegisterDto>)
+        fileInfoList : 첨부 파일 정보 리스트 (List<FileRequest>)
          */
         @Schema(description = "게시글 제목", example = "게시글 제목 입니다.")
         private String title;
@@ -25,17 +29,20 @@ public class QuestionRegister {
         private String content;
         @Schema(description = "게시글 첨부 링크 목록")
         private List<RegisterDto> linkList;
+        @Schema(description = "게시글 첨부 파일 목록")
+        private List<FileRequest> fileInfoList;
+
+        /**
+         * 업무 관리 게시글 등록 요청 정보로 업무 관리 게시글 엔티티를 만드는 메서드
+         *
+         * @param registerDto 엄무 관리 게시글 등록 요청 정보
+         * @return Question Entity
+         */
+        public static Question toEntity(QuestionRegister.Request registerDto) {
+            return Question.builder().title(registerDto.getTitle())
+                .content(registerDto.getContent()).build();
+        }
     }
 
-    /**
-     * 업무 관리 게시글 등록 요청 정보로 업무 관리 게시글 엔티티를 만드는 메서드
-     *
-     * @param registerDto 엄무 관리 게시글 등록 요청 정보
-     * @return Question Entity
-     */
-    public static Question toEntity(QuestionRequest.RegisterDto registerDto) {
-        return Question.builder().title(registerDto.getTitle())
-            .content(registerDto.getContent()).build();
-    }
 
 }
