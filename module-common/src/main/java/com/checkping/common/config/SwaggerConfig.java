af9840d1c29abbc36ded3835361a8082058052a1
oowtl <oowtl7425@gmail.com>
2025-01-21 22:41:51 +0900

    feat : 스웨거 서버 설정 추가
    - https://dev.flowssync.com
    
    연관 이슈
    - #218

--- module-common/src/main/java/com/checkping/common/config/SwaggerConfig.java
+++ module-common/src/main/java/com/checkping/common/config/SwaggerConfig.java
@@ -13,7 +13,8 @@ import org.springframework.context.annotation.Configuration;
 @OpenAPIDefinition(
         servers = {
                 @Server(url = "http://localhost:8080", description = "로컬 서버"),
-                @Server(url = "https://api.flowssync.com", description = "개발 서버"),
+                @Server(url = "https://test.flowssync.com", description = "개발 서버"),
+                @Server(url = "https://api.flowssync.com", description = "운영 서버"),
         })
 @Configuration
 public class SwaggerConfig {
