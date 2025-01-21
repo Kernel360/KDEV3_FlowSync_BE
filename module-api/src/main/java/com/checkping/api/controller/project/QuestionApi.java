e58e0cc6cd57e46fc0df0428e971a97e55477c0f
oowtl <oowtl7425@gmail.com>
2025-01-21 15:37:28 +0900

    feat : QuestionApi - Swagger 설명 변경
    - 업무 관리 게시글을 질문 게시글로 변경
    
    연관 이슈
    - #201

--- module-api/src/main/java/com/checkping/api/controller/project/QuestionApi.java
+++ module-api/src/main/java/com/checkping/api/controller/project/QuestionApi.java
@@ -16,42 +16,42 @@ import org.springframework.web.bind.annotation.RequestPart;
 import org.springframework.web.multipart.MultipartFile;
 
 
-@Tag(name = "업무 관리 게시판 API(QuestionController)", description = "업무 관리 게시판 API 입니다.")
+@Tag(name = "Question API(QuestionController)", description = "질문 게시판 API 입니다.")
 public interface QuestionApi {
 
-    @Operation(summary = "업무 관리 게시글 등록", description = "업무 관리 게시글을 등록하는 기능입니다. 파일 첨부도 같이 받습니다.")
+    @Operation(summary = "질문 게시글 등록", description = "질문 게시글을 등록하는 기능입니다.")
     BaseResponse<QuestionItemDto> register(
         @Parameter(description = "등록 게시글 정보", required = true, content = @Content(mediaType = MediaType.APPLICATION_JSON_VALUE)) @RequestPart(value = "content") QuestionRequest.RegisterDto request,
         @Parameter(description = "등록 첨부 파일", content = @Content(mediaType = MediaType.MULTIPART_FORM_DATA_VALUE)) @RequestPart(required = false, value = "fileList") List<MultipartFile> fileList);
 
-    @Operation(summary = "업무 관리 게시글 목록 조회", description = "업무 관리 게시글 목록을 조회하는 기능입니다.")
+    @Operation(summary = "질문 게시글 목록 조회", description = "질문 게시글 목록을 조회하는 기능입니다.")
     BaseResponse<List<QuestionListDto>> getTaskBoardList(
         @Parameter(description = "게시글 유형 - null 가능") String boardCategory,
         @Parameter(description = "게시글 상태 - null 가능") String boardStatus,
         @Parameter(description = "게시글 검색어")String keyword);
 
-    @Operation(summary = "업무 관리 게시글 상세 조회", description = "업무 관리 게시글을 조회하는 기능입니다.")
+    @Operation(summary = "질문 게시글 상세 조회", description = "질문 게시글을 조회하는 기능입니다.")
     BaseResponse<QuestionItemDto> getTaskBoard(@Parameter(description = "게시글 ID") Long postId);
 
-    @Operation(summary = "업무 관리 게시글 수정", description = "업무 관리 게시글을 수정하는 기능입니다.")
+    @Operation(summary = "질문 게시글 수정", description = "질문 게시글을 수정하는 기능입니다.")
     BaseResponse<QuestionItemDto> updateTaskBoard(@Parameter(description = "게시글 ID") Long postId,
         @Parameter(description = "게시글 수정 정보 Dto") QuestionRequest.UpdateDto request);
 
-    @Operation(summary = "업무 관리 게시글 소프트 삭제", description = "업무 관리 게시글을 약한 삭제를 하는 기능입니다..")
+    @Operation(summary = "질문 게시글 소프트 삭제", description = "질문 게시글을 약한 삭제를 하는 기능입니다..")
     BaseResponse<QuestionListDto> deleteSoftTaskBoard(
         @Parameter(description = "게시글 ID") Long postId);
 
-    @Operation(summary = "업무 관리 게시글 댓글 등록", description = "업무 관리 게시글의 댓글을 등록하는 기능입니다.")
+    @Operation(summary = "질문 게시글 댓글 등록", description = "질문 게시글의 댓글을 등록하는 기능입니다.")
     BaseResponse<QuestionCommentDto> registerComment(
         @Parameter(description = "게시글 ID") Long postId,
         @Parameter(description = "게시글 댓글 등록 Dto") QuestionCommentRequest.RegisterDto request);
 
-    @Operation(summary = "업무 관리 게시글 댓글 소프트 삭제", description = "업무 관리 게시글의 댓글을 소프트 삭제하는 기능입니다.")
+    @Operation(summary = "질문 게시글 댓글 소프트 삭제", description = "질문 게시글의 댓글을 소프트 삭제하는 기능입니다.")
     BaseResponse<QuestionCommentDto> deleteSoft(
         @Parameter(description = "게시글 ID") Long postId,
         @Parameter(description = "게시글 댓글 ID") Long commentId);
 
-    @Operation(summary = "업무 관리 게시글 댓글 수정", description = "업무 관리 게시글의 댓글을 수정하는 기능입니다.")
+    @Operation(summary = "질문 게시글 댓글 수정", description = "질문 게시글의 댓글을 수정하는 기능입니다.")
     BaseResponse<QuestionCommentDto> updateComment(
         @Parameter(description = "게시글 ID") Long postId,
         @Parameter(description = "게시글 댓글 ID") Long commentId,
