da6bcdc34c34b89d8711076c3b5f118917ff06a4
oowtl <oowtl7425@gmail.com>
2025-01-22 22:34:56 +0900

    fix : 컨벤션 수정
    연관 이슈
    - #226

--- module-api/src/main/java/com/checkping/api/auth/config/CustomSecurityConfig.java
+++ module-api/src/main/java/com/checkping/api/auth/config/CustomSecurityConfig.java
@@ -4,6 +4,8 @@ package com.checkping.api.auth.config;
 import com.checkping.api.auth.filter.CustomLogoutFilter;
 import com.checkping.api.auth.filter.LoginFilter;
 import com.checkping.service.member.util.JwtUtil;
+import java.util.Arrays;
+import java.util.Collections;
 import java.util.List;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -20,9 +22,6 @@ import org.springframework.web.cors.CorsConfiguration;
 import org.springframework.web.cors.CorsConfigurationSource;
 import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
 
-import java.util.Arrays;
-import java.util.Collections;
-
 @Configuration
 @EnableWebSecurity
 public class CustomSecurityConfig {
@@ -31,7 +30,8 @@ public class CustomSecurityConfig {
     private final AuthenticationConfiguration authenticationConfiguration;
     private final JwtUtil jwtUtil;
 
-    public CustomSecurityConfig(AuthenticationConfiguration authenticationConfiguration, JwtUtil jwtUtil) {
+    public CustomSecurityConfig(AuthenticationConfiguration authenticationConfiguration,
+        JwtUtil jwtUtil) {
 
         this.authenticationConfiguration = authenticationConfiguration;
         this.jwtUtil = jwtUtil;
@@ -39,7 +39,8 @@ public class CustomSecurityConfig {
 
     //AuthenticationManager Bean 등록
     @Bean
-    public AuthenticationManager authenticationManager(AuthenticationConfiguration configuration) throws Exception {
+    public AuthenticationManager authenticationManager(AuthenticationConfiguration configuration)
+        throws Exception {
 
         return configuration.getAuthenticationManager();
     }
@@ -50,29 +51,30 @@ public class CustomSecurityConfig {
 
         //CORS 설정
         http
-                .cors((cors) -> cors.configurationSource(corsConfiguration()));
+            .cors((cors) -> cors.configurationSource(corsConfiguration()));
         //csrf disable
         http
-                .csrf((auth) -> auth.disable());
+            .csrf((auth) -> auth.disable());
 
         //From 로그인 방식 disable
         http
-                .formLogin((auth) -> auth.disable());
+            .formLogin((auth) -> auth.disable());
 
         //http basic 인증 방식 disable
         http
-                .httpBasic((auth) -> auth.disable());
+            .httpBasic((auth) -> auth.disable());
 
         http
-                .headers(headers->headers.frameOptions(HeadersConfigurer.FrameOptionsConfig::sameOrigin));
+            .headers(
+                headers -> headers.frameOptions(HeadersConfigurer.FrameOptionsConfig::sameOrigin));
 
         //경로별 인가 작업
         http
-                .authorizeHttpRequests((auth) -> auth
-                        .requestMatchers("/login").permitAll()
-                        .requestMatchers("/reissue").permitAll()
-                        //anyRequest().authenticated());
-                        .anyRequest().permitAll()); // TODO MVP에서는 일단 모든 경로 권한 필요 없음, 추후 경로 별 권한 설정
+            .authorizeHttpRequests((auth) -> auth
+                .requestMatchers("/login").permitAll()
+                .requestMatchers("/reissue").permitAll()
+                //anyRequest().authenticated());
+                .anyRequest().permitAll()); // TODO MVP에서는 일단 모든 경로 권한 필요 없음, 추후 경로 별 권한 설정
 
         //기능 테스트 위해서 일시적인 주석처리 2025/01/15
 //        http
@@ -80,30 +82,36 @@ public class CustomSecurityConfig {
 
         // 필터 추가 LoginFilter()는 인자를 받음 (AuthenticationManager() 메소드에 authenticationConfiguration 객체를 넣어야 함) 따라서 등록 필요
         http
-                .addFilterAt(new LoginFilter(authenticationManager(authenticationConfiguration), jwtUtil), UsernamePasswordAuthenticationFilter.class);
+            .addFilterAt(
+                new LoginFilter(authenticationManager(authenticationConfiguration), jwtUtil),
+                UsernamePasswordAuthenticationFilter.class);
 
         http
-                .addFilterBefore(new CustomLogoutFilter(jwtUtil), LogoutFilter.class);
+            .addFilterBefore(new CustomLogoutFilter(jwtUtil), LogoutFilter.class);
 
         //세션 설정
         http
-                .sessionManagement((session) -> session
-                        .sessionCreationPolicy(SessionCreationPolicy.STATELESS));
+            .sessionManagement((session) -> session
+                .sessionCreationPolicy(SessionCreationPolicy.STATELESS));
 
         return http.build();
     }
 
     //CORS 설정
-    public CorsConfigurationSource corsConfiguration(){
+    public CorsConfigurationSource corsConfiguration() {
         CorsConfiguration configuration = new CorsConfiguration();
         configuration.setAllowedMethods(Collections.singletonList("*"));
 //        configuration.setAllowedOriginPatterns(Collections.singletonList("*"));
-        configuration.setAllowedOrigins(List.of("https://www.flowssync.com", "http://localhost:3000", "https://dev.flowssync.com", "https://api.flowssync.com", "https://test.flowssync.com"));
+        configuration.setAllowedOrigins(
+            List.of("https://www.flowssync.com", "http://localhost:3000",
+                "https://dev.flowssync.com", "https://api.flowssync.com",
+                "https://test.flowssync.com"));
         configuration.setAllowCredentials(true);
         configuration.setAllowedHeaders(Collections.singletonList("*"));
-        configuration.setExposedHeaders(Arrays.asList("Authorization", "Content-Type", "Content-Disposition"));
+        configuration.setExposedHeaders(
+            Arrays.asList("Authorization", "Content-Type", "Content-Disposition"));
         UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
         source.registerCorsConfiguration("/**", configuration);
         return source;
-        }
     }
+}
