d2960a9afc2f1f1d1071596c7aa2e776d8989109
oowtl <oowtl7425@gmail.com>
2025-01-23 16:39:37 +0900

    feat : QuestionRegister.Response 생성
    - 승인 일시 삭제
    - toDto 를 위한 setter 사용
    
    연관 이슈
    - #210

--- module-service/src/main/java/com/checkping/dto/question/QuestionRegister.java
+++ module-service/src/main/java/com/checkping/dto/question/QuestionRegister.java
@@ -1,15 +1,22 @@
 package com.checkping.dto.question;
 
 import com.checkping.common.utils.FileRequest;
+import com.checkping.common.utils.FileResponse;
 import com.checkping.domain.question.Question;
 import com.checkping.domain.question.Question.Category;
+import com.checkping.domain.question.Question.Status;
+import com.checkping.dto.question.comment.QuestionCommentResponse.QuestionCommentDto;
 import com.checkping.dto.question.link.QuestionLinkRegister;
+import com.checkping.dto.question.link.QuestionLinkResponse.QuestionLinkDto;
 import com.checkping.exception.question.QuestionCategoryException;
+import com.fasterxml.jackson.annotation.JsonFormat;
 import io.swagger.v3.oas.annotations.media.Schema;
+import java.time.LocalDateTime;
 import java.util.List;
 import lombok.AccessLevel;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
+import lombok.Setter;
 import lombok.ToString;
 
 @NoArgsConstructor(access = AccessLevel.PRIVATE)
@@ -51,6 +58,62 @@ public class QuestionRegister {
             return Question.generate(projectId, registerDto.getProgressStepId(),
                 registerDto.getTitle(), registerDto.getContent(),
                 convertCategory(registerDto.getCategory()));
+        }
+    }
+
+    @Getter
+    @Setter(value = AccessLevel.PRIVATE)
+    public static class Response {
+
+        /*
+        id : 게시글 고유 ID
+        number : 게시글 번호
+        title : 게시글 제목
+        content : 게시글 본문 내용
+        regAt : 게시글 작성 일시
+        editAt : 게시글 마지막 수정 일시
+        category : 게시글 카테고리 (enum, String)
+        status : 게시글 상태 (enum, String)
+        commentList : 게시글 댓글 리스트
+        linkList : 게시글 첨부 링크 리스트
+        fileList : 게시글 첨부 파일 리스트
+         */
+        @Schema(description = "게시글 번호")
+        private Long id;
+        @Schema(description = "게시글 번호")
+        private Integer number;
+        @Schema(description = "게시글 제목", example = "게시글 제목 입니다.")
+        private String title;
+        @Schema(description = "게시글 본문", example = "게시글 본문 입니다.")
+        private String content;
+        @Schema(description = "등록 일시")
+        @JsonFormat(pattern = "yyyy-MM-dd HH:mm:ss")
+        private LocalDateTime regAt;
+        @Schema(description = "수정 일시")
+        @JsonFormat(pattern = "yyyy-MM-dd HH:mm:ss")
+        private LocalDateTime editAt;
+        @Schema(description = "게시글 유형")
+        private Category category;
+        @Schema(description = "게시글 상태")
+        private Status status;
+        @Schema(description = "게시글 댓글 목록")
+        private List<QuestionCommentDto> commentList;
+        @Schema(description = "게시글 첨부 링크 목록")
+        private List<QuestionLinkDto> linkList;
+        @Schema(description = "게시글 첨부 파일 목록")
+        private List<FileResponse> fileList;
+
+        public static Response toDto(Question question) {
+            Response questionDto = new Response();
+            questionDto.setId(question.getId());
+            questionDto.setNumber(question.getNumber());
+            questionDto.setTitle(question.getTitle());
+            questionDto.setContent(question.getContent());
+            questionDto.setRegAt(question.getRegAt());
+            questionDto.setEditAt(question.getEditAt());
+            questionDto.setCategory(question.getCategory());
+            questionDto.setStatus(question.getStatus());
+            return questionDto;
         }
     }
 
