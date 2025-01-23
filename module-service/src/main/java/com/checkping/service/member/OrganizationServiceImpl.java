e3fe03a90b312d72544811236cf11d5ffdba4350
jaseongkim <jaseong4281@gmail.com>
2025-01-23 16:32:51 +0900

    feat: 서비스 로직 수정
    BrCertificateUrl() 빈문자열 or Null 체크

--- module-service/src/main/java/com/checkping/service/member/OrganizationServiceImpl.java
+++ module-service/src/main/java/com/checkping/service/member/OrganizationServiceImpl.java
@@ -79,7 +79,7 @@ public class OrganizationServiceImpl implements OrganizationService {
         return PageInfo.Response.<OrganizationGet.Response>builder()
                 .dtoList(dtoList)
                 .pageRequest(pageRequest)
-                .totalCount((int)totalCount)
+                .totalCount((int) totalCount)
                 .build();
     }
 
@@ -93,8 +93,13 @@ public class OrganizationServiceImpl implements OrganizationService {
         Optional<Organization> result = organizationRepository.findById(id);
         Organization organization = result.orElseThrow(OrganizationNotFoundEntityException::new);
 
-        // 저장 파일명
-        String saveName = organization.getBrCertificateUrl().split("\\|")[0];
+        // 기존 파일이 있다면 삭제
+        if (organization.getBrCertificateUrl() != null && !organization.getBrCertificateUrl().isEmpty()) {
+            // 저장 파일명
+            String saveName = organization.getBrCertificateUrl().split("\\|")[0];
+            // 기존 파일 삭제
+            s3FileRepositoryImpl.deleteFile(saveName);
+        }
 
         // 수정 파일 등록
         if (file != null) {
@@ -111,9 +116,6 @@ public class OrganizationServiceImpl implements OrganizationService {
         );
 
         Organization updateOrganization = organizationRepository.save(organization);
-
-        // 기존 파일 삭제
-        s3FileRepositoryImpl.deleteFile(saveName);
 
         OrganizationUpdate.Response.toDto(updateOrganization);
 
