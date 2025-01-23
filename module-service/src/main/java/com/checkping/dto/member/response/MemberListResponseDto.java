7faa937ecf37cd4d1c86bc564b9be3a0b024001d
heegu123 <kookminkhg@kookmin.ac.kr>
2025-01-23 13:33:58 +0900

    feat: MemberListResponseDto에 메타데이터 추가
    - MemberListResponseDto에 'meta' 필드를 추가하여 페이징 관련 메타데이터 반환 기능 구현
      - totalElements, isFirstPage, isLastPage, totalPages, pageSize, currentPage 정보 포함
    - - 공통적으로 사용하는 meta 데이터를 관리하기 위해 MetaResponse 클래스 생성
        - totalElements, isFirstPage, isLastPage, totalPages, pageSize, currentPage 포함
        - Page 객체를 기반으로 MetaResponse 생성 가능 (fromPage 메서드)
        - meta 데이터를 Map으로 변환하는 toMap 메서드 추가
    
      - MemberListResponseDto에서 meta 로직을 MetaResponse로 대체
    
      - 공통 모듈을 활용
    
    - 기존 데이터 리스트와 메타데이터를 동시에 반환하도록 구조 변경
    
    #Issue: 197

--- module-service/src/main/java/com/checkping/dto/member/response/MemberListResponseDto.java
+++ module-service/src/main/java/com/checkping/dto/member/response/MemberListResponseDto.java
@@ -1,46 +1,30 @@
 package com.checkping.dto.member.response;
 
+import com.checkping.common.response.PageMetaResponse;
 import com.checkping.domain.member.Member;
 import lombok.Getter;
 import org.springframework.data.domain.Page;
 
 import java.util.List;
-import java.util.stream.Collectors;
 
 @Getter
 public class MemberListResponseDto {
 
     private final List<MemberResponseDto> members;
-    private long totalElements;
-    private int totalPages;
+    private final PageMetaResponse meta;
 
-    public MemberListResponseDto(List<MemberResponseDto> members) {
+    public MemberListResponseDto(List<MemberResponseDto> members, PageMetaResponse meta) {
         this.members = members;
+        this.meta = meta;
     }
 
     public static MemberListResponseDto fromEntityPage(Page<Member> page) {
         List<MemberResponseDto> memberDtos = page.getContent().stream()
                 .map(MemberResponseDto::fromEntity)
                 .toList();
-        return new MemberListResponseDto(memberDtos, page.getTotalElements(), page.getTotalPages());
-    }
 
-    // 생성자와 Getter 추가
-    public MemberListResponseDto(List<MemberResponseDto> members, long totalElements, int totalPages) {
-        this.members = members;
-        this.totalElements = totalElements;
-        this.totalPages = totalPages;
-    }
+        PageMetaResponse meta = PageMetaResponse.fromPage(page);
 
-    public List<MemberResponseDto> getMembers() {
-        return members;
-    }
-
-    public long getTotalElements() {
-        return totalElements;
-    }
-
-    public int getTotalPages() {
-        return totalPages;
+        return new MemberListResponseDto(memberDtos, meta);
     }
 }
