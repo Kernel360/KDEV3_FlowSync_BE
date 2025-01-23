7883acd42eac12d8edb02b9791a6178d61c1dbd4
oowtl <oowtl7425@gmail.com>
2025-01-23 20:48:22 +0900

    feat : QuestionFileRegister.Request 생성
    - FileRequest 를 QuestionFileRegister 를 변환하는 책임을 QuestionFileStore 에서 부담을 하고 있어서 변경
    
    연관 이슈
    - #210

--- module-service/src/main/java/com/checkping/dto/question/file/QuestionFileRegister.java
+++ module-service/src/main/java/com/checkping/dto/question/file/QuestionFileRegister.java
@@ -1,5 +1,7 @@
 package com.checkping.dto.question.file;
 
+import com.checkping.common.utils.FileRequest;
+import com.checkping.domain.question.Question;
 import com.checkping.domain.question.QuestionFile;
 import java.util.List;
 import lombok.AccessLevel;
@@ -8,6 +10,53 @@ import lombok.NoArgsConstructor;
 
 @NoArgsConstructor(access = AccessLevel.PRIVATE)
 public class QuestionFileRegister {
+
+    @Getter
+    public static class Request {
+
+        /*
+        originalName : 원본 파일명
+        saveName : 저장 파일명
+        url : 저장 URL
+        size : 파일 Size
+         */
+        private String originalName;
+        private String saveName;
+        private String url;
+        private long size;
+
+        /**
+         * FileRequest Dto -> QuestionFile Entity
+         *
+         * @param question Question Entity
+         * @param request  FileRequest Dto (첨부 파일)
+         * @return QuestionFile Entity
+         */
+        public static QuestionFile toEntity(Question question, FileRequest request) {
+
+            return QuestionFile.generate(question, request.originalName(), request.saveName(),
+                request.url(), request.size());
+        }
+
+
+        /**
+         * FileRequest Dto List -> QuestionFile Entity List
+         *
+         * @param question Question Entity
+         * @param requests FileRequest Dto List (첨부 파일 리스트)
+         * @return List<QuestionFile> Entity List
+         */
+        public static List<QuestionFile> toEntity(Question question,
+            List<FileRequest> requests) {
+
+            // Check null or empty
+            if (requests == null || requests.isEmpty()) {
+                return List.of();
+            }
+
+            return requests.stream().map(request -> toEntity(question, request)).toList();
+        }
+    }
 
     @Getter
     public static class Response {
@@ -56,9 +105,7 @@ public class QuestionFileRegister {
                 return List.of();
             }
 
-            return questionFiles.stream()
-                .map(QuestionFileRegister.Response::toDto)
-                .toList();
+            return questionFiles.stream().map(QuestionFileRegister.Response::toDto).toList();
         }
     }
 }
