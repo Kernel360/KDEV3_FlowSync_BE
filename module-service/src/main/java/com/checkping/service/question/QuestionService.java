7ec1bf06278318caab4a7028cac19125a119be75
oowtl <oowtl7425@gmail.com>
2025-01-23 16:41:08 +0900

    feat : QuestionService 수정
    - register의 반환 타입을 QuestionRegister.Response 로 변경
    
    연관 이슈
    - #210

--- module-service/src/main/java/com/checkping/service/question/QuestionService.java
+++ module-service/src/main/java/com/checkping/service/question/QuestionService.java
@@ -1,14 +1,15 @@
 package com.checkping.service.question;
 
+import com.checkping.dto.question.QuestionRegister;
 import com.checkping.dto.question.QuestionRegister.Request;
 import com.checkping.dto.question.QuestionRequest;
 import com.checkping.dto.question.QuestionRequest.UpdateDto;
-import com.checkping.dto.question.QuestionResponse.QuestionListDto;
 import com.checkping.dto.question.QuestionResponse.QuestionItemDto;
+import com.checkping.dto.question.QuestionResponse.QuestionListDto;
 import java.util.List;
 
 public interface QuestionService {
-    QuestionItemDto register(Long projectId, Request request);
+    QuestionRegister.Response register(Long projectId, Request request);
     List<QuestionListDto> getQuestionList(QuestionRequest.SearchCondition searchCondition);
     QuestionItemDto getQuestionById(Long taskBoardId);
     QuestionListDto deleteSoft (Long taskBoardId);
