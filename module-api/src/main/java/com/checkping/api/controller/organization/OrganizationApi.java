41deca4f5b3f3ab24a93c073986a0a26f03e2a5e
jaseongkim <jaseong4281@gmail.com>
2025-01-23 13:49:32 +0900

    feat: 스웨거 문서 수정
    전체 조회 api 문서 수정

--- module-api/src/main/java/com/checkping/api/controller/organization/OrganizationApi.java
+++ module-api/src/main/java/com/checkping/api/controller/organization/OrganizationApi.java
@@ -1,5 +1,6 @@
 package com.checkping.api.controller.organization;
 
+import com.checkping.common.dto.PageInfo;
 import com.checkping.common.response.BaseResponse;
 import com.checkping.dto.OrganizationCreate;
 import com.checkping.dto.OrganizationGet;
@@ -14,7 +15,6 @@ import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RequestPart;
 import org.springframework.web.multipart.MultipartFile;
 
-import java.util.List;
 import java.util.UUID;
 
 @Tag(name = "업체 API(OrganizationController)", description = "업체 API 입니다.")
@@ -30,9 +30,10 @@ public interface OrganizationApi {
     BaseResponse<OrganizationGet.Response> getOrganization(@Parameter(description = "업체 ID")UUID organizationId);
 
     @Operation(summary = "업체 전체 조회", description = "업체 조회 기능입니다.")
-    BaseResponse<List<OrganizationGet.Response>> getAllByTypeOrganization(
+    BaseResponse<PageInfo.Response<OrganizationGet.Response>> getListOrganization(
             @Parameter(description = "업체 타입") @RequestParam(required = false) String type,
-            @Parameter(description = "업체 상태") @RequestParam(required = false) String status
+            @Parameter(description = "업체 상태") @RequestParam(required = false) String status,
+            @Parameter(description = "page & size") PageInfo.Request pageRequest
     );
 
     @Operation(summary = "업체 수정", description = "업체 정보 수정 기능입니다.")
