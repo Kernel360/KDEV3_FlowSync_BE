2921f3be98525f2889022a4b63e0b7e346174bf2
oowtl <oowtl7425@gmail.com>
2025-01-23 20:41:35 +0900

    feat : QuestionLinkRegister
    - Response 생성
    - toEntity 위치 변경 - 본래 컨벤션으로 정한 위치
    
    연관 이슈
    - #210

--- module-service/src/main/java/com/checkping/dto/question/link/QuestionLinkRegister.java
+++ module-service/src/main/java/com/checkping/dto/question/link/QuestionLinkRegister.java
@@ -25,34 +25,81 @@ public class QuestionLinkRegister {
         @Schema(description = "게시글 링크 url")
         private String url;
 
+        /**
+         * QuestionLink 등록 요청 Dto -> QuestionLink Entity
+         *
+         * @param question Question Entity
+         * @param request  QuestionLinkRegister.Request Dto
+         * @return QuestionLink Entity
+         */
+        public static QuestionLink toEntity(Question question, QuestionLinkRegister.Request request) {
+            return QuestionLink.builder()
+                .name(request.getName())
+                .url(request.getUrl())
+                .question(question)
+                .build();
+        }
+
+        /**
+         * QuestionLink 등록 요청 Dto 리스트 -> QuestionLink Entity 리스트
+         *
+         * @param question Question Entity
+         * @param requests QuestionLinkRegister.Request List
+         * @return List<QuestionLink> Entity List
+         */
+        public static List<QuestionLink> toEntity(Question question,
+            List<QuestionLinkRegister.Request> requests) {
+            return requests.stream()
+                .map(request -> toEntity(question, request))
+                .toList();
+        }
     }
 
-    /**
-     * QuestionLink 등록 요청 Dto -> QuestionLink Entity
-     *
-     * @param question Question Entity
-     * @param request  QuestionLinkRegister.Request Dto
-     * @return QuestionLink Entity
-     */
-    public static QuestionLink toEntity(Question question, QuestionLinkRegister.Request request) {
-        return QuestionLink.builder()
-            .name(request.getName())
-            .url(request.getUrl())
-            .question(question)
-            .build();
+    public static class Response {
+        /*
+        id : 게시글 링크 아이디
+        projectId : 프로젝트 아이디
+        name : 게시글 링크 이름
+        url : 게시글 링크 url
+         */
+
+        private Long id;
+        private Long projectId;
+        private String name;
+        private String url;
+
+        /**
+         * QuestionLink Entity -> QuestionLinkRegister.Response Dto
+         *
+         * @param link QuestionLink Entity
+         * @return QuestionLinkRegister.Response Dto
+         */
+        public static QuestionLinkRegister.Response toDto(QuestionLink link) {
+            QuestionLinkRegister.Response dto = new QuestionLinkRegister.Response();
+            dto.id = link.getId();
+            // TODO : projectId 실제로 찾아서 가져오기 (getProject() 로 변경 예정이기 때문)
+            dto.projectId = link.getQuestion().getProjectId();
+            dto.name = link.getName();
+            dto.url = link.getUrl();
+            return dto;
+        }
+
+        /**
+         * QuestionLink Entity 리스트 -> QuestionLinkRegister.Response Dto 리스트
+         *
+         * @param links QuestionLink Entity List
+         * @return List<QuestionLinkRegister.Response> Dto List
+         */
+        public static List<QuestionLinkRegister.Response> toDto(List<QuestionLink> links) {
+            // Check null or empty
+            if (links == null || links.isEmpty()) {
+                return List.of();
+            }
+
+            return links.stream()
+                .map(QuestionLinkRegister.Response::toDto)
+                .toList();
+        }
     }
 
-    /**
-     * QuestionLink 등록 요청 Dto 리스트 -> QuestionLink Entity 리스트
-     *
-     * @param question Question Entity
-     * @param requests QuestionLinkRegister.Request List
-     * @return List<QuestionLink> Entity List
-     */
-    public static List<QuestionLink> toEntity(Question question,
-        List<QuestionLinkRegister.Request> requests) {
-        return requests.stream()
-            .map(request -> toEntity(question, request))
-            .toList();
-    }
 }
