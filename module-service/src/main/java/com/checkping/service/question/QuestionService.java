11951419d019c19f01b4a8b1026031ab9a59e11f
oowtl <oowtl7425@gmail.com>
2025-01-24 22:52:19 +0900

    feat : 프로젝트 단계 별 조회 서비스 생성
    - QuestionService
    
    연관 이슈
    - #252

--- module-service/src/main/java/com/checkping/service/question/QuestionService.java
+++ module-service/src/main/java/com/checkping/service/question/QuestionService.java
@@ -1,5 +1,6 @@
 package com.checkping.service.question;
 
+import com.checkping.dto.question.QuestionCounter;
 import com.checkping.dto.question.QuestionRegister;
 import com.checkping.dto.question.QuestionRegister.Request;
 import com.checkping.dto.question.QuestionRequest.UpdateDto;
@@ -7,6 +8,7 @@ import com.checkping.dto.question.QuestionResponse.QuestionItemDto;
 import com.checkping.dto.question.QuestionResponse.QuestionListDto;
 import com.checkping.dto.question.QuestionSearch;
 import com.checkping.dto.question.QuestionSearchCondition;
+import java.util.List;
 
 public interface QuestionService {
     QuestionRegister.Response register(Long projectId, Request request);
@@ -15,4 +17,5 @@ public interface QuestionService {
     QuestionListDto deleteSoft (Long taskBoardId);
     QuestionListDto deleteHard(Long taskBoardId);
     QuestionItemDto update(Long taskBoardId, UpdateDto request);
+    List<QuestionCounter.Response> countByProgressStep(Long projectId);
 }
