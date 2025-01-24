ab97652789358bd7ad90c180b697567084c59fb2
oowtl <oowtl7425@gmail.com>
2025-01-24 14:53:57 +0900

    feat : QuestionRepository 쿼리 메소드 생성
    - QueryMethod 로 검색 기능 구현 (추후 QueryDSL 구현 예정)
    
    연관 이슈
    - #242

--- module-infra/src/main/java/com/checkping/infra/repository/question/QuestionRepository.java
+++ module-infra/src/main/java/com/checkping/infra/repository/question/QuestionRepository.java
@@ -3,7 +3,6 @@ package com.checkping.infra.repository.question;
 import com.checkping.domain.question.Question;
 import com.checkping.domain.question.Question.Category;
 import com.checkping.domain.question.Question.Status;
-import java.util.List;
 import java.util.Optional;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
@@ -12,23 +11,6 @@ import org.springframework.stereotype.Repository;
 
 @Repository
 public interface QuestionRepository extends JpaRepository<Question, Long> {
-
-    List<Question> findByCategory(Category category);
-
-    List<Question> findByStatus(Status status);
-
-    List<Question> findByTitleContaining(String taskBoardTitle);
-
-    List<Question> findByCategoryAndStatus(Category category,
-        Status status);
-
-    List<Question> findQuestionByCategoryAndTitleContaining(Category category,
-        String title);
-
-    List<Question> findQuestionByStatusAndTitleContaining(Status status, String title);
-
-    List<Question> findByCategoryAndStatusAndTitleContaining(Category category,
-        Status status, String title);
 
     Optional<Question> findById(Long id);
 
