66d9b8c735ce29635768b5b74f3926fc878a18db
oowtl <oowtl7425@gmail.com>
2025-01-24 09:52:40 +0900

    feat : QuestionRepository.findByProjectId 추가
    - 페이징 처리를 위한 Pageable 추가 및 Page<Question> 반환
    
    연관 이슈
    - #242

--- module-infra/src/main/java/com/checkping/infra/repository/question/QuestionRepository.java
+++ module-infra/src/main/java/com/checkping/infra/repository/question/QuestionRepository.java
@@ -5,6 +5,8 @@ import com.checkping.domain.question.Question.Category;
 import com.checkping.domain.question.Question.Status;
 import java.util.List;
 import java.util.Optional;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
@@ -29,4 +31,20 @@ public interface QuestionRepository extends JpaRepository<Question, Long> {
         Status status, String title);
 
     Optional<Question> findById(Long id);
+
+    Page<Question> findByProjectId(Long projectId, Pageable pageable);
+
+    Page<Question> findByProjectIdAndCategory(Long projectId, Category category, Pageable pageable);
+
+    Page<Question> findByProjectIdAndStatus(Long projectId, Status status, Pageable pageable);
+
+    Page<Question> findByProjectIdAndCategoryAndStatus(Long projectId, Category category, Status status, Pageable pageable);
+
+    Page<Question> findByProjectIdAndTitleContaining(Long projectId, String title, Pageable pageable);
+
+    Page<Question> findByProjectIdAndCategoryAndTitleContaining(Long projectId, Category category, String title, Pageable pageable);
+
+    Page<Question> findByProjectIdAndStatusAndTitleContaining(Long projectId, Status status, String title, Pageable pageable);
+
+    Page<Question> findByProjectIdAndCategoryAndStatusAndTitleContaining(Long projectId, Category category, Status status, String title, Pageable pageable);
 }
