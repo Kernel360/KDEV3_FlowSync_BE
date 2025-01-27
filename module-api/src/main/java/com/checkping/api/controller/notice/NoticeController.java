47d03c8f74d47b8e226382e70f8cf6868428bdc6
quickestfunctioning <snc01004@naver.com>
2025-01-27 15:55:12 +0900

    feature : NoticeController 수정
     - NoticeController에서 NoticeApi의 메서드 오버라이딩

--- module-api/src/main/java/com/checkping/api/controller/notice/NoticeController.java
+++ module-api/src/main/java/com/checkping/api/controller/notice/NoticeController.java
@@ -7,12 +7,15 @@ import com.checkping.dto.notice.response.NoticeCreateResponse;
 import com.checkping.dto.notice.response.NoticeGetListResponse;
 import com.checkping.dto.notice.response.NoticeResponse;
 import com.checkping.service.notice.NoticeServiceImpl;
+import jakarta.websocket.OnError;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
+@Slf4j
 @RestController
 @RequiredArgsConstructor
 public class NoticeController implements NoticeApi {
@@ -20,12 +23,14 @@ public class NoticeController implements NoticeApi {
     @Autowired
     private NoticeServiceImpl noticeService;
 
+    @Override
     @PostMapping("/admins/notices")
     public BaseResponse<NoticeCreateResponse> registerNotice(@RequestBody NoticeCreateRequest noticeCreateRequest) {
         NoticeCreateResponse noticeCreateResponse = noticeService.registerNotice(noticeCreateRequest);
         return BaseResponse.success(noticeCreateResponse);
     }
 
+    @Override
     @PatchMapping("/admins/notices/{noticeid}")
     public BaseResponse<NoticeResponse> updateNotice(
             @PathVariable Long noticeid,
@@ -34,6 +39,7 @@ public class NoticeController implements NoticeApi {
         return BaseResponse.success(noticeResponse);
     }
 
+    @Override
     @DeleteMapping("/admins/notices/{noticeid}")
     public BaseResponse<NoticeResponse> deleteNotice(
             @PathVariable Long noticeid
@@ -42,12 +48,14 @@ public class NoticeController implements NoticeApi {
         return BaseResponse.success(noticeDeleteResponse);
     }
 
+    @Override
     @GetMapping("/notices")
     public BaseResponse<List<NoticeGetListResponse>> findAllNotices(){
         List<NoticeGetListResponse> result = noticeService.findAllNotices();
         return BaseResponse.success(result);
     }
 
+    @Override
     @GetMapping("/notices/{noticeid}")
     public BaseResponse<NoticeResponse> getNotice(
             @PathVariable Long noticeid
