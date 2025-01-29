3b7dd61342dd5e80a902d670d0aaf4ed187193de
oowtl <oowtl7425@gmail.com>
2025-01-29 21:25:01 +0900

    feat : 결재 파일 Store - List 단위로 받을 수 있도록변경
    - 결재 파일 Store - List 단위로 받을 수 있도록변경
    
    관련 이슈
    - #229

--- module-infra/src/main/java/com/checkping/infra/repository/approval/file/ApprovalFileStoreImpl.java
+++ module-infra/src/main/java/com/checkping/infra/repository/approval/file/ApprovalFileStoreImpl.java
@@ -1,6 +1,7 @@
 package com.checkping.infra.repository.approval.file;
 
 import com.checkping.domain.approval.ApprovalFile;
+import java.util.List;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Component;
 
@@ -10,8 +11,24 @@ public class ApprovalFileStoreImpl implements ApprovalFileStore {
 
     private final ApprovalFileRepository approvalFileRepository;
 
+    /**
+     * ApprovalFile 저장
+     * @param approvalFile  ApprovalFile
+     * @return  ApprovalFile
+     */
     @Override
     public ApprovalFile store(ApprovalFile approvalFile) {
         return approvalFileRepository.save(approvalFile);
+    }
+
+    /**
+     * ApprovalFile List 저장
+     *
+     * @param approvalFiles ApprovalFile List
+     * @return  ApprovalFile List
+     */
+    @Override
+    public List<ApprovalFile> store(List<ApprovalFile> approvalFiles) {
+        return approvalFiles.stream().map(approvalFileRepository::save).toList();
     }
 }
