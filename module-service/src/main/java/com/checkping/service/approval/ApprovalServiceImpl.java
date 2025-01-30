414fbd7e86f9f68d2d54123c0f59a7ea062ae1a7
oowtl <oowtl7425@gmail.com>
2025-01-30 20:27:27 +0900

    feat : 결재 목록 조회 서비스 메서드 추가
    - 서비스 인터페이스 및 구현체 추가
    
    관련 이슈
    - #268

--- module-service/src/main/java/com/checkping/service/approval/ApprovalServiceImpl.java
+++ module-service/src/main/java/com/checkping/service/approval/ApprovalServiceImpl.java
@@ -4,13 +4,18 @@ import com.checkping.domain.approval.Approval;
 import com.checkping.domain.approval.ApprovalFile;
 import com.checkping.domain.approval.ApprovalLink;
 import com.checkping.dto.approval.ApprovalRegister;
+import com.checkping.dto.approval.ApprovalSearch;
+import com.checkping.dto.approval.ApprovalSearchCondition;
 import com.checkping.dto.approval.file.ApprovalFileRegister;
 import com.checkping.dto.approval.link.ApprovalLinkRegister;
+import com.checkping.info.approval.ApprovalSearchInfo;
+import com.checkping.infra.repository.approval.ApprovalReader;
 import com.checkping.infra.repository.approval.ApprovalStore;
 import com.checkping.infra.repository.approval.file.ApprovalFileStore;
 import com.checkping.infra.repository.approval.link.ApprovalLinkStore;
 import java.util.List;
 import lombok.RequiredArgsConstructor;
+import org.springframework.data.domain.Page;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
@@ -21,6 +26,7 @@ public class ApprovalServiceImpl implements ApprovalService {
     private final ApprovalStore approvalStore;
     private final ApprovalFileStore approvalFileStore;
     private final ApprovalLinkStore approvalLinkStore;
+    private final ApprovalReader approvalReader;
 
     @Transactional
     @Override
@@ -35,19 +41,41 @@ public class ApprovalServiceImpl implements ApprovalService {
         Approval approval = approvalStore.store(init);
 
         // FileRequest -> Entity
-        List<ApprovalFile> approvalFiles = ApprovalFileRegister.Request.toEntity(approval, request.getFileInfoList());
+        List<ApprovalFile> approvalFiles = ApprovalFileRegister.Request.toEntity(approval,
+            request.getFileInfoList());
         // Save approvalFiles
         approvalFileStore.store(approvalFiles);
         // Add approvalFiles to approval
         approval.addFiles(approvalFiles);
 
         // LinkRequest -> Entity
-        List<ApprovalLink> approvalLinks = ApprovalLinkRegister.Request.toEntity(approval, request.getLinkList());
+        List<ApprovalLink> approvalLinks = ApprovalLinkRegister.Request.toEntity(approval,
+            request.getLinkList());
         // Save approvalLinks
         approvalLinkStore.store(approvalLinks);
         // Add approvalLinks to approval
         approval.addLinks(approvalLinks);
 
         return ApprovalRegister.Response.toDto(approval);
+    }
+
+    /**
+     * 결재 목록 조회
+     *
+     * @param request 결재 목록 조회 조건
+     * @return 결재 목록 조회 결과
+     */
+    @Transactional(readOnly = true)
+    public ApprovalSearch.Response search(Long projectId, ApprovalSearchCondition request) {
+
+        // ApprovalSearchCondition -> ApprovalSearchInfo.SearchCondition
+        ApprovalSearchInfo.SearchCondition searchCondition = ApprovalSearchCondition.toInfo(
+            request);
+
+        // Search Approval
+        Page<Approval> approvals = approvalReader.getApprovals(projectId, searchCondition);
+
+        // Approval Entity List -> ApprovalItem Dto List
+        return ApprovalSearch.Response.toDto(approvals);
     }
 }
