fe17c6ef974fce35da5010e3b360d04c127fbb02
oowtl <oowtl7425@gmail.com>
2025-01-30 20:28:12 +0900

    feat : 결재 목록 조회 컨트롤러 엔드포인트 추가
    - 컨트롤러 인터페이스(swagger) 및 구현체 추가
    
    관련 이슈
    - #268

--- module-api/src/main/java/com/checkping/api/controller/approval/ApprovalController.java
+++ module-api/src/main/java/com/checkping/api/controller/approval/ApprovalController.java
@@ -2,16 +2,23 @@ package com.checkping.api.controller.approval;
 
 import com.checkping.common.response.BaseResponse;
 import com.checkping.dto.approval.ApprovalRegister;
+import com.checkping.dto.approval.ApprovalSearch;
+import com.checkping.dto.approval.ApprovalSearchCondition;
 import com.checkping.service.approval.ApprovalService;
+import jakarta.validation.constraints.Min;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.validation.annotation.Validated;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @Slf4j
+@Validated
 @RestController
 @RequestMapping("/projects/{projectId}/approvals")
 @RequiredArgsConstructor
@@ -26,6 +33,25 @@ public class ApprovalController implements ApprovalApi {
         @RequestBody ApprovalRegister.Request request) {
 
         ApprovalRegister.Response response = approvalService.register(projectId, request);
+
+        return BaseResponse.success(response);
+    }
+
+    @GetMapping
+    @Override
+    public BaseResponse<ApprovalSearch.Response> search(@PathVariable Long projectId,
+        @RequestParam(required = false) Long progressId,
+        @RequestParam(required = false) String status,
+        @RequestParam(required = false) String keyword,
+        @Min(0) @RequestParam(defaultValue = "1") Integer currentPage,
+        @RequestParam(defaultValue = "10") Integer pageSize) {
+
+        // Create ApprovalSearchCondition
+        ApprovalSearchCondition request = new ApprovalSearchCondition(progressId, status, keyword,
+            currentPage, pageSize);
+
+        // Search Approval
+        ApprovalSearch.Response response = approvalService.search(projectId, request);
 
         return BaseResponse.success(response);
     }
