b813f26b0dbe26dd635e913d1d77074708ee9030
oowtl <oowtl7425@gmail.com>
2025-01-30 20:18:18 +0900

    feat : 결재 Repository - 결재 검색 쿼리메서드 추가
    - 진행단계, 결재 상태, 결재 제목에 따른 쿼리메서드 추가
    
    관련 이슈
    - #268

--- module-infra/src/main/java/com/checkping/infra/repository/approval/ApprovalRepository.java
+++ module-infra/src/main/java/com/checkping/infra/repository/approval/ApprovalRepository.java
@@ -1,10 +1,28 @@
 package com.checkping.infra.repository.approval;
 
 import com.checkping.domain.approval.Approval;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
 public interface ApprovalRepository extends JpaRepository<Approval, Long> {
+
+    Page<Approval> findByProjectId(Long projectId, Pageable pageable);
+
+    Page<Approval> findByProjectIdAndTitleContaining(Long projectId, String title, Pageable pageable);
+
+    Page<Approval> findByProjectIdAndStatus(Long projectId, Approval.ApprovalStatus status, Pageable pageable);
+
+    Page<Approval> findByProjectIdAndProgressStepId(Long projectId, Long progressStepId, Pageable pageable);
+
+    Page<Approval> findByProjectIdAndTitleContainingAndStatus(Long projectId, String title, Approval.ApprovalStatus status, Pageable pageable);
+
+    Page<Approval> findByProjectIdAndTitleContainingAndProgressStepId(Long projectId, String title, Long progressStepId, Pageable pageable);
+
+    Page<Approval> findByProjectIdAndProgressStepIdAndStatus(Long projectId, Long progressStepId, Approval.ApprovalStatus status, Pageable pageable);
+
+    Page<Approval> findByProjectIdAndTitleContainingAndProgressStepIdAndStatus(Long projectId, String title, Long progressStepId, Approval.ApprovalStatus status, Pageable pageable);
 
 }
