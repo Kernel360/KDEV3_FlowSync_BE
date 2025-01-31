77ae6174a74a8975a803aad79d35c0df2ba8571b
oowtl <oowtl7425@gmail.com>
2025-01-31 15:42:03 +0900

    feat : 댓글 생성 Dto 변경 및 매개변수 이름 변경
    - 매개변수 순서변경
    - QuestionCommentRegister.Request.toEntity 로 변경
    
    관련 이슈
    - #282

--- module-service/src/main/java/com/checkping/service/question/comment/QuestionCommentService.java
+++ module-service/src/main/java/com/checkping/service/question/comment/QuestionCommentService.java
@@ -1,13 +1,13 @@
 package com.checkping.service.question.comment;
 
+import com.checkping.dto.question.comment.QuestionCommentRegister;
 import com.checkping.dto.question.comment.QuestionCommentRequest;
-import com.checkping.dto.question.comment.QuestionCommentRequest.RegisterDto;
 import com.checkping.dto.question.comment.QuestionCommentResponse.QuestionCommentDto;
 
 public interface QuestionCommentService {
 
     QuestionCommentDto register(
-        Long taskBoardId, RegisterDto request);
+        Long projectId, QuestionCommentRegister.Request request);
 
     QuestionCommentDto deleteSoft(Long taskBoardId,
         Long taskBoardCommentId);
