31964be1e305df52353284a6ce3531a5df354f83
quickestfunctioning <snc01004@naver.com>
2025-01-31 19:07:45 +0900

    공지사항 검색 기능 추가
    - 카테고리만으로 검색 기능 구현
    - 카테고리와 키워드 함께 검색 기능 구현

--- module-service/src/main/java/com/checkping/service/notice/NoticeService.java
+++ module-service/src/main/java/com/checkping/service/notice/NoticeService.java
@@ -20,7 +20,7 @@ public interface NoticeService {
 
     NoticeResponse getNotice(Long noticeid);
 
-    Page<NoticeGetListResponse> searchNotices(String keyword, String category, Pageable pageable);  // 카테고리 파라미터 추가
+    Page<NoticeGetListResponse> searchNotices(String keyword, String category, Pageable pageable);
 }
 
-}
+
