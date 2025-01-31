36f8e988f913ec243307236525dfb29d1f37fe8d
oowtl <oowtl7425@gmail.com>
2025-01-31 20:27:30 +0900

    feat : QuestionCommentService.registerReComment
    - 대댓글 기능
    
    관련 이슈
    - #282

--- module-service/src/main/java/com/checkping/service/question/comment/QuestionCommentService.java
+++ module-service/src/main/java/com/checkping/service/question/comment/QuestionCommentService.java
@@ -3,11 +3,16 @@ package com.checkping.service.question.comment;
 import com.checkping.dto.question.comment.QuestionCommentRegister;
 import com.checkping.dto.question.comment.QuestionCommentRequest;
 import com.checkping.dto.question.comment.QuestionCommentResponse.QuestionCommentDto;
+import com.checkping.dto.question.comment.QuestionReCommentRegister;
 
 public interface QuestionCommentService {
 
     QuestionCommentRegister.Response register(
         Long projectId, QuestionCommentRegister.Request request);
+
+    QuestionReCommentRegister.Response registerReComment(
+        Long projectId, Long commentId,
+        QuestionReCommentRegister.Request request);
 
     QuestionCommentDto deleteSoft(Long taskBoardId,
         Long taskBoardCommentId);
