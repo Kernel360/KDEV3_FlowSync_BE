a6be77fef6f688c768da95047fe307484ed36ec4
oowtl <oowtl7425@gmail.com>
2025-02-01 23:28:31 +0900

    feat : 댓글 정보 조회 시 부모 댓글인지 여부를 체크하는 속성 추가
    - 응답 Dto에 부모 댓글인지 여부를 체크하는 속성 추가
    - Jackson 라이브러리의 인식을 위해서 getIsParent 메서드를 추가
    
    관련 이슈
    - #290

--- module-service/src/main/java/com/checkping/dto/question/comment/QuestionCommentGet.java
+++ module-service/src/main/java/com/checkping/dto/question/comment/QuestionCommentGet.java
@@ -2,12 +2,15 @@ package com.checkping.dto.question.comment;
 
 import com.checkping.common.utils.DateTimeUtils;
 import com.checkping.domain.question.QuestionComment;
+import com.fasterxml.jackson.annotation.JsonInclude;
+import com.fasterxml.jackson.annotation.JsonInclude.Include;
 import io.swagger.v3.oas.annotations.media.Schema;
 import java.util.List;
 import lombok.AccessLevel;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 
+@JsonInclude(Include.ALWAYS)
 @NoArgsConstructor(access = AccessLevel.PRIVATE)
 public class QuestionCommentGet {
 
@@ -20,6 +23,7 @@ public class QuestionCommentGet {
         regAt : 작성 일시
         editAt : 수정 일시
         parentId : 부모 댓글 아이디
+        isParent : 부모 댓글인지 여부 체크 T/F
          */
         @Schema(description = "질문 게시글 댓글 아이디")
         private Long id;
@@ -31,7 +35,8 @@ public class QuestionCommentGet {
         private String editAt;
         @Schema(description = "질문 게시글 댓글 부모 댓글 아이디")
         private Long parentId;
-
+        @Schema(description = "부모 댓글인지 여부 체크 T/F")
+        private boolean isParent;
         /**
          * 질문 게시글 댓글 엔티티를 응답 정보로 변환하는 메서드
          *
@@ -44,7 +49,15 @@ public class QuestionCommentGet {
             response.content = comment.getContent();
             response.regAt = DateTimeUtils.format(comment.getRegAt());
             response.editAt = DateTimeUtils.format(comment.getEditAt());
-            response.parentId = comment.getParent() == null ? null : comment.getParent().getId();
+            response.parentId = null;
+            response.isParent = true;
+
+            // 부모 댓글이 있는 경우
+            if (comment.getParent() != null) {
+                response.parentId = comment.getParent().getId();
+                response.isParent = false;
+            }
+
             return response;
         }
 
@@ -58,6 +71,17 @@ public class QuestionCommentGet {
             return comments.stream()
                 .map(Response::toDto)
                 .toList();
+        }
+
+        /**
+         * 질문 게시글이 부모 댓글인지 여부를 반환한다.
+         * 부모 댓글인 경우 true, 자식 댓글인 경우 false를 반환한다.
+         * JackSon 라이브러리가 isParent 필드를 인식하도록 하기 위해 get 메서드를 추가한다.
+         *
+         * @return 부모 댓글인지 여부
+         */
+        public boolean getIsParent() {
+            return isParent;
         }
     }
 
