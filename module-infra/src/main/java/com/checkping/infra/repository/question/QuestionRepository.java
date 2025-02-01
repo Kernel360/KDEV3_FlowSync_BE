5192da7fccca8c2d5dfa33255fcd582172cef175
oowtl <oowtl7425@gmail.com>
2025-02-01 23:13:21 +0900

    feat : QuestionRepository findByIdWithComments 쿼리 메서드 추가
    - 댓글과 대댓글이 같은 계층에 있어야 한다.
    - 댓글의 대댓글은 댓글의 밑으로 정렬되어 보여야 한다.
    
    관련 이슈
    - #290

--- module-infra/src/main/java/com/checkping/infra/repository/question/QuestionRepository.java
+++ module-infra/src/main/java/com/checkping/infra/repository/question/QuestionRepository.java
@@ -6,6 +6,8 @@ import java.util.Optional;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
 @Repository
@@ -13,21 +15,33 @@ public interface QuestionRepository extends JpaRepository<Question, Long> {
 
     Optional<Question> findById(Long id);
 
+    @Query("SELECT q FROM Question q " +
+        "LEFT JOIN FETCH q.commentList c " +
+        "WHERE q.id = :questionId " +
+        "ORDER BY COALESCE(c.parent.id, c.id) , c.regAt ASC")
+    Optional<Question> findByIdWithComments(@Param("questionId") Long id);
+
     Page<Question> findByProjectId(Long projectId, Pageable pageable);
 
-    Page<Question> findByProjectIdAndProgressStepId(Long projectId, Long progressStepId, Pageable pageable);
+    Page<Question> findByProjectIdAndProgressStepId(Long projectId, Long progressStepId,
+        Pageable pageable);
 
     Page<Question> findByProjectIdAndStatus(Long projectId, Status status, Pageable pageable);
 
-    Page<Question> findByProjectIdAndProgressStepIdAndStatus(Long projectId, Long progressId, Status status, Pageable pageable);
+    Page<Question> findByProjectIdAndProgressStepIdAndStatus(Long projectId, Long progressId,
+        Status status, Pageable pageable);
 
-    Page<Question> findByProjectIdAndTitleContaining(Long projectId, String title, Pageable pageable);
+    Page<Question> findByProjectIdAndTitleContaining(Long projectId, String title,
+        Pageable pageable);
 
-    Page<Question> findByProjectIdAndProgressStepIdAndTitleContaining(Long projectId, Long progressStepId, String title, Pageable pageable);
+    Page<Question> findByProjectIdAndProgressStepIdAndTitleContaining(Long projectId,
+        Long progressStepId, String title, Pageable pageable);
 
-    Page<Question> findByProjectIdAndStatusAndTitleContaining(Long projectId, Status status, String title, Pageable pageable);
+    Page<Question> findByProjectIdAndStatusAndTitleContaining(Long projectId, Status status,
+        String title, Pageable pageable);
 
-    Page<Question> findByProjectIdAndProgressStepIdAndStatusAndTitleContaining(Long projectId, Long progressStepId, Status status, String title, Pageable pageable);
+    Page<Question> findByProjectIdAndProgressStepIdAndStatusAndTitleContaining(Long projectId,
+        Long progressStepId, Status status, String title, Pageable pageable);
 
     Long countByProjectId(Long projectId);
 
