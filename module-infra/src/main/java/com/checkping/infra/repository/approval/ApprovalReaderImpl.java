61a764f0c71d2929f86213af93228c31e0e3300d
oowtl <oowtl7425@gmail.com>
2025-02-02 02:42:09 +0900

    feat : 결재 조회 메서드 생성
    - findById 를 사용
    
    관련 이슈
    - #293

--- module-infra/src/main/java/com/checkping/infra/repository/approval/ApprovalReaderImpl.java
+++ module-infra/src/main/java/com/checkping/infra/repository/approval/ApprovalReaderImpl.java
@@ -2,6 +2,7 @@ package com.checkping.infra.repository.approval;
 
 import com.checkping.domain.approval.Approval;
 import com.checkping.info.approval.ApprovalSearchInfo;
+import java.util.Optional;
 import lombok.RequiredArgsConstructor;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.PageRequest;
@@ -13,6 +14,11 @@ import org.springframework.stereotype.Component;
 public class ApprovalReaderImpl implements ApprovalReader {
 
     private final ApprovalRepository approvalRepository;
+
+    @Override
+    public Optional<Approval> getById(Long approvalId) {
+        return approvalRepository.findById(approvalId);
+    }
 
     @Override
     public Page<Approval> getApprovals(Long projectId,
