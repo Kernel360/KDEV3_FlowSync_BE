33972bb1e3f8f3c4421636fa7bca03fbbe461173
a121255 <a132258@naver.com>
2025-02-02 14:55:00 +0900

    feature : 네이티브 쿼리 추가, UpdateDetailsDto 생성
    - 프로젝트 정보와 함께 개발자 및 고객 조직 ID를 조회하는 네이티브 SQL 쿼리 추가
    - 쿼리 결과를 ProjectUpdateDetailsDto로 매핑

---
+++ module-infra/src/main/java/com/checkping/infra/dto/ProjectUpdateDetailsDto.java
@@ -0,0 +1,25 @@
+package com.checkping.infra.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+
+import java.util.Date;
+
+@Getter
+@Builder
+@AllArgsConstructor
+public class ProjectUpdateDetailsDto {
+    private final Long id;
+    private final String name;
+    private final String description;
+    private final String detail;
+    private final String status;
+    private final String managementStep;
+    private final Long progressStepId;
+    private final Date startAt;
+    private final Date closeAt;
+    private final Long devOwnerId;
+    private final Long developerOrgId;
+    private final Long customerOrgId;
+}
