3e038ef1c771128b0b3edf570ba8c7b2548486a9
oowtl <oowtl7425@gmail.com>
2025-02-02 22:07:17 +0900

    feature : 결재 게시글 본문 Dto 수정
    - String 으로 들어오는 것을 파싱하여 List 로 바꾸는 메서드 추가 : Question 에서는 QuestionGet 에 존재하였지만, 해당 메서드가 하는 역할과 toDto, toEntity 가 Dto 에 있는 팀의 컨벤션을 고려했을 때 본문 Dto 에 메서드가 존재해야 알맞다고 판단
    
    관련 이슈
    - #230

--- module-service/src/main/java/com/checkping/dto/approval/ApprovalContent.java
+++ module-service/src/main/java/com/checkping/dto/approval/ApprovalContent.java
@@ -1,7 +1,11 @@
 package com.checkping.dto.approval;
 
+import com.checkping.exception.approval.ApprovalContentParsingException;
 import com.fasterxml.jackson.annotation.JsonCreator;
 import com.fasterxml.jackson.annotation.JsonProperty;
+import com.fasterxml.jackson.core.type.TypeReference;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import java.util.List;
 import lombok.Getter;
 
 @Getter
@@ -18,5 +22,22 @@ public class ApprovalContent {
     public ApprovalContent(@JsonProperty("type") String type, @JsonProperty("data") Object data) {
         this.type = type;
         this.data = data;
+    }
+
+
+    /**
+     * content 를 List<ApprovalContent> 로 변환하는 메서드
+     *
+     * @param content content
+     * @return List<ApprovalContent>
+     */
+    public static List<ApprovalContent> toContentList(String content) {
+        ObjectMapper objectMapper = new ObjectMapper();
+        try {
+            return objectMapper.readValue(content, new TypeReference<List<ApprovalContent>>() {
+            });
+        } catch (Exception e) {
+            throw new ApprovalContentParsingException();
+        }
     }
 }
