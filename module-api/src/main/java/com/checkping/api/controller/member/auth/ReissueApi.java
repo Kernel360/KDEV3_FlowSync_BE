3432a07b938c52be5a7303d4f0faa82c2e06441e
heegu123 <kookminkhg@kookmin.ac.kr>
2025-02-02 20:09:41 +0900

    리프레시토큰 발급 Get으로 변경

--- module-api/src/main/java/com/checkping/api/controller/member/auth/ReissueApi.java
+++ module-api/src/main/java/com/checkping/api/controller/member/auth/ReissueApi.java
@@ -5,7 +5,7 @@ import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.tags.Tag;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
-import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.GetMapping;
 
 @Tag(name = "토큰 재발급 Reissue API", description = "토큰 재발급 API입니다.")
 public interface ReissueApi {
@@ -14,6 +14,6 @@ public interface ReissueApi {
             summary = "토큰 재발급",
             description = "Access Token과 Refresh Token을 재발급하는 기능입니다."
     )
-    @PostMapping("/reissue")
+    @GetMapping("/reissue")
     BaseResponse<?> reissue(HttpServletRequest request, HttpServletResponse response);
 }
