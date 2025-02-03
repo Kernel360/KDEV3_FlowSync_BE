5ec2b524419bc389a2d112d84b7641561f2ddf00
jaseongkim <jaseong4281@gmail.com>
2025-02-03 23:03:15 +0900

    feat: 결재요청 접근 권한 수정
    프로젝트 결재 담당자만 결재 가능

--- module-api/src/main/java/com/checkping/api/auth/interceptor/AuthInterceptor.java
+++ module-api/src/main/java/com/checkping/api/auth/interceptor/AuthInterceptor.java
@@ -38,14 +38,14 @@ public class AuthInterceptor implements HandlerInterceptor {
 
             boolean exist = memberByProjectService.existsByMemberIdAndProjectId(getAuthenticatedUserId(), projectId);
 
-            if(!exist) {
-                throw new BaseException("프로젝트 접근 권한이 없습니다.",ErrorCode.BAD_REQUEST);
+            if (!exist) {
+                throw new BaseException("프로젝트 접근 권한이 없습니다.", ErrorCode.BAD_REQUEST);
             }
             return true;
         }
 
         // 질문/결재 게시판
-        if (requestURI.matches("^/projects/\\d+/questions.*$")||
+        if (requestURI.matches("^/projects/\\d+/questions.*$") ||
                 requestURI.matches("^/projects/\\d+/approvals.*$")) {
 
             log.info("게시판 interceptor");
@@ -55,20 +55,30 @@ public class AuthInterceptor implements HandlerInterceptor {
 
             boolean exist = memberByProjectService.existsByMemberIdAndProjectId(getAuthenticatedUserId(), projectId);
 
-            if(!exist) {
-                throw new BaseException("게시판 접근 권한이 없습니다.",ErrorCode.BAD_REQUEST);
+            if (!exist) {
+                throw new BaseException("게시판 접근 권한이 없습니다.", ErrorCode.BAD_REQUEST);
             }
             return true;
         }
 
         //결재 요청
-
-
+//        if (requestURI.matches("^/projects/\\d+/questions.*$")) {
+//
+//            log.info("결재 요청 interceptor");
+//
+//            String[] uriParts = requestURI.split("/");
+//            Long projectId = Long.parseLong(uriParts[2]); // 세 번째 마지막 값이 projectId
+//
+//            boolean exist = memberByProjectService.isMemberApprovalForProject(getAuthenticatedUserId(), projectId);
+//
+//            if (!exist) {
+//                throw new BaseException("결재 요청 권한이 없습니다.", ErrorCode.BAD_REQUEST);
+//            }
+//            return true;
+//        }
 
         return true;
     }
-
-
 
     /**
      * 로그인한 유저 ID 가져오기
@@ -76,13 +86,11 @@ public class AuthInterceptor implements HandlerInterceptor {
      * @return 유저 ID
      */
     public Long getAuthenticatedUserId() {
-
         Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
 
         if (authentication != null && authentication.getPrincipal() instanceof CustomUserDetails userDetails) {
             return userDetails.getId();
         }
-
         return null;
     }
 }
--- module-service/src/main/java/com/checkping/service/permission/MemberByProjectService.java
+++ module-service/src/main/java/com/checkping/service/permission/MemberByProjectService.java
@@ -3,4 +3,7 @@ package com.checkping.service.permission;
 public interface MemberByProjectService {
 
     boolean existsByMemberIdAndProjectId(Long memberId, Long projectId);
+
+    boolean isMemberApprovalForProject(Long memberId, Long projectId);
+
 }
