b18c7f495c2fefeb90b390f779283a657a27bcdd
quickestfunctioning <snc01004@naver.com>
2025-02-03 19:47:41 +0900

    TODO 주석 수정

--- module-service/src/main/java/com/checkping/service/notice/NoticeServiceImpl.java
+++ module-service/src/main/java/com/checkping/service/notice/NoticeServiceImpl.java
@@ -85,4 +85,4 @@ public class NoticeServiceImpl implements NoticeService {
 }
 
 //TODO : 모든 DTO, 엔티티에서 관리자아이디 제거 (DB에서도 해당 컬럼 전부 제거)
-//TODO : 긴급 공지사항이 일정 기간이 지나면 일반으로 변경되며 고정도 풀림
+//TODO : 예외처리를 포함한 리팩토링
