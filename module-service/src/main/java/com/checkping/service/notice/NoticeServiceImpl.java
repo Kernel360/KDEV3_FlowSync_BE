c7ab6a4ac12e318ffa5591f7948f6bf5a89d41db
quickestfunctioning <snc01004@naver.com>
2025-02-03 19:17:54 +0900

    TODO 주석 추가

--- module-service/src/main/java/com/checkping/service/notice/NoticeServiceImpl.java
+++ module-service/src/main/java/com/checkping/service/notice/NoticeServiceImpl.java
@@ -83,3 +83,8 @@ public class NoticeServiceImpl implements NoticeService {
     }
 
 }
+
+//TODO : 공지사항 생성 시에 요청받는 파라미터 값에서 관리자아이디 제거
+//TODO : 1페이지가 아닌 페이지에서도 긴급 공지사항은 항상 맨 위에 고정
+//TODO : 긴급 공지사항의 갯수를 제한
+//TODO : 긴급 공지사항이 일정 기간이 지나면 일반으로 변경되며 고정도 풀림
