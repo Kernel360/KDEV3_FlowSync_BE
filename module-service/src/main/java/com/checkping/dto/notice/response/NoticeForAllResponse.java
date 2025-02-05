2a2341abbe59c02cf05f43043d84e91100a30c43
quickestfunctioning <snc01004@naver.com>
2025-02-05 21:32:51 +0900

    관리자 권한 없이 공지사항 조회 수정
    - 관리자 권한 없이 공지사항 목록조회 및 상세조회 시 isDeleted 컬럼을 응답에 미포함시키도록 수정

---
+++ module-service/src/main/java/com/checkping/dto/notice/response/NoticeForAllResponse.java
@@ -0,0 +1,61 @@
+package com.checkping.dto.notice.response;
+
+
+import com.checkping.common.exception.BaseException;
+import com.checkping.domain.notice.Notice;
+import com.checkping.dto.notice.NoticeContent;
+import com.fasterxml.jackson.core.type.TypeReference;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import io.swagger.v3.oas.annotations.media.Schema;
+import lombok.Builder;
+import lombok.Getter;
+
+import java.time.LocalDateTime;
+import java.util.List;
+
+@Getter
+@Builder
+public class NoticeForAllResponse {
+
+    @Schema(description = "공지사항 아이디", example = "1")
+    private Long id;
+
+    @Schema(description = "공지사항 글 제목", example = "드릴말씀")
+    private String title;
+
+    @Schema(description = "공지사항 글 내용", example = "ABCDEFG")
+    private List<NoticeContent> content;
+
+    @Schema(description = "공지사항 글 카테고리", example = "MAINTENANCE")
+    private Notice.Category category;
+
+    @Schema(description = "공지사항 글 중요도", example = "EMERGENCY")
+    private Notice.Priority priority;
+
+    @Schema(description = "생성 날짜", example = "2025-01-27T13:43:33.4716151")
+    private LocalDateTime regAt;
+
+    @Schema(description = "수정 날짜", example = "2025-01-28T13:43:33.4716151")
+    private LocalDateTime updatedAt;
+
+    public static NoticeForAllResponse toDto(Notice notice){
+        return NoticeForAllResponse.builder()
+                .id(notice.getId())
+                .title(notice.getTitle())
+                .content(convertJsonToContentList(notice.getContent()))
+                .category(notice.getCategory())
+                .priority(notice.getPriority())
+                .regAt(notice.getRegAt())
+                .updatedAt(notice.getUpdatedAt())
+                .build();
+    }
+
+    private static List<NoticeContent> convertJsonToContentList(String json) {
+        ObjectMapper objectMapper = new ObjectMapper();
+        try {
+            return objectMapper.readValue(json, new TypeReference<List<NoticeContent>>() {});
+        } catch (Exception e) {
+            throw new BaseException();
+        }
+    }
+}
--- module-service/src/main/java/com/checkping/service/notice/NoticeService.java
+++ module-service/src/main/java/com/checkping/service/notice/NoticeService.java
@@ -4,10 +4,7 @@ import com.checkping.domain.notice.Notice;
 import com.checkping.dto.notice.request.NoticeCreateRequest;
 import com.checkping.dto.notice.request.NoticeSearchRequest;
 import com.checkping.dto.notice.request.NoticeUpdateRequest;
-import com.checkping.dto.notice.response.NoticeCreateResponse;
-import com.checkping.dto.notice.response.NoticeGetListResponse;
-import com.checkping.dto.notice.response.NoticeListResponse;
-import com.checkping.dto.notice.response.NoticeResponse;
+import com.checkping.dto.notice.response.*;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 
@@ -19,10 +16,10 @@ public interface NoticeService {
 
     NoticeResponse deleteNotice(Long noticeid);
 
-    NoticeResponse getNotice(Long noticeid);
+    NoticeForAllResponse getNotice(Long noticeid);
 
     NoticeResponse getAdminNotice(Long noticeid);
-    NoticeListResponse getNotices(NoticeSearchRequest noticeSearchRequest);
+    NoticeListForAllResponse getNotices(NoticeSearchRequest noticeSearchRequest);
 
     NoticeListResponse getAdminNotices(NoticeSearchRequest noticeSearchRequest);
 
