863fb5a64738b17906e0cf0c348b032d572169a0
jaseongkim <jaseong4281@gmail.com>
2025-02-05 09:31:17 +0900

    feat: 정렬기능 추가

--- module-service/src/main/java/com/checkping/service/member/OrganizationServiceImpl.java
+++ module-service/src/main/java/com/checkping/service/member/OrganizationServiceImpl.java
@@ -16,6 +16,7 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.PageRequest;
 import org.springframework.data.domain.Pageable;
+import org.springframework.data.domain.Sort;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 import org.springframework.web.multipart.MultipartFile;
@@ -67,7 +68,10 @@ public class OrganizationServiceImpl implements OrganizationService {
     @Override
     public PageInfo.Response<OrganizationGet.Response> getListOrganization(String type, String status, PageInfo.Request pageRequest) {
 
-        Pageable pageable = PageRequest.of(pageRequest.getCurrentPage() - 1, pageRequest.getPageSize());
+        Pageable pageable = PageRequest.of(
+                pageRequest.getCurrentPage() - 1,
+                pageRequest.getPageSize(),
+                Sort.by("id").descending());
 
         Organization.Type validType = checkType(type);
         Organization.Status validStatus = checkStatus(status);
