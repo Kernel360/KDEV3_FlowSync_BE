edc45813a8350c7167c2871c24b7e4e6a296d921
oowtl <oowtl7425@gmail.com>
2025-02-07 03:06:14 +0900

    feat : 결재 수정 서비스 인터페이스
    관련 이슈
    - #365

--- module-service/src/main/java/com/checkping/service/approval/ApprovalService.java
+++ module-service/src/main/java/com/checkping/service/approval/ApprovalService.java
@@ -5,6 +5,7 @@ import com.checkping.dto.approval.ApprovalGet;
 import com.checkping.dto.approval.ApprovalRegister;
 import com.checkping.dto.approval.ApprovalSearch;
 import com.checkping.dto.approval.ApprovalSearchCondition;
+import com.checkping.dto.approval.ApprovalUpdate;
 import com.checkping.dto.approval.comment.ApprovalCommentRegister;
 import com.checkping.dto.approval.comment.ApprovalReCommentRegister;
 
@@ -15,6 +16,8 @@ public interface ApprovalService {
     ApprovalSearch.Response search(Long projectId, ApprovalSearchCondition request);
 
     ApprovalGet.Response get(Long projectId, Long approvalId);
+
+    ApprovalUpdate.Response update(Long projectId, Long approvalId, ApprovalUpdate.Request request);
 
     ApprovalCommentRegister.Response registerComment(Long projectId, Long approvalId,
         ApprovalCommentRegister.Request request);
