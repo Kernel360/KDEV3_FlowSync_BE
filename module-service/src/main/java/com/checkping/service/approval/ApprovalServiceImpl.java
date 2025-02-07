7f127bc2e13ee13cc32e81d8facde13d603d22bd
oowtl <oowtl7425@gmail.com>
2025-02-07 16:22:02 +0900

    feat : 결재 수정 API - 응답 Dto 구조 변경
    - 클라이언트에서 해당 API 를 사용하지 않음에 따라서 응답 형식 변경
    
    관련 이슈
    - #365

--- module-service/src/main/java/com/checkping/service/approval/ApprovalServiceImpl.java
+++ module-service/src/main/java/com/checkping/service/approval/ApprovalServiceImpl.java
@@ -183,8 +183,6 @@ public class ApprovalServiceImpl implements ApprovalService {
 
         // Save approvalLinks
         approvalLinkStore.store(newLinks);
-        // Add approvalLinks to approval
-        approval.updateLinks(newLinks);
 
         // 첨부 파일 처리
         // 1. approval 에 속한 파일 중에서 request 에 없는 것은 삭제 처리 한다.
@@ -207,8 +205,6 @@ public class ApprovalServiceImpl implements ApprovalService {
 
         // Save approvalFiles
         approvalFileStore.store(newFiles);
-        // Update approvalFiles to approval
-        approval.updateFiles(newFiles);
 
         return ApprovalUpdate.Response.toDto(approval);
     }
