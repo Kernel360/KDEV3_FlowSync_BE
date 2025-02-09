abb910a8bea4e6d3d0a01cb6935a558dcb5ec8db
oowtl <oowtl7425@gmail.com>
2025-02-10 01:23:27 +0900

    feat : 결재 CustomRepository 인터페이스 매개변수 타입 수정
    - 메서드의 매개변수를 내부 클래스까지 명시
    
    관련 이슈
    - #403

--- module-infra/src/main/java/com/checkping/infra/repository/approval/ApprovalCustomRepository.java
+++ module-infra/src/main/java/com/checkping/infra/repository/approval/ApprovalCustomRepository.java
@@ -2,7 +2,7 @@ package com.checkping.infra.repository.approval;
 
 import com.checkping.domain.approval.Approval;
 import com.checkping.info.approval.ApprovalCountProjection;
-import com.checkping.info.approval.ApprovalSearchInfo.SearchCondition;
+import com.checkping.info.approval.ApprovalSearchInfo;
 import java.util.List;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
@@ -11,6 +11,6 @@ public interface ApprovalCustomRepository {
 
     List<ApprovalCountProjection> countByProgressStep(Long projectId);
 
-    Page<Approval> getByCondition(Long projectId, SearchCondition searchCondition,
+    Page<Approval> getByCondition(Long projectId, ApprovalSearchInfo.SearchCondition searchCondition,
         Pageable pageable);
 }
