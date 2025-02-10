963a8a3cde6d875f98633038b2725700ed88b4d5
quickestfunctioning <snc01004@naver.com>
2025-02-10 11:12:21 +0900

    공지사항 조회 수정
    - 권한에 따라 soft delete 된 공지사항을 확인할 수 있는지, isDeleted 컬럼을 확인할 수 있는지에 대하여 코드 리팩토링

--- module-infra/src/main/java/com/checkping/infra/repository/notice/NoticeRepository.java
+++ module-infra/src/main/java/com/checkping/infra/repository/notice/NoticeRepository.java
@@ -33,9 +33,9 @@ public interface NoticeRepository extends JpaRepository<Notice, Long> {
     @Query("SELECT n FROM Notice n WHERE (:category IS NULL OR n.category = :category) " +
             "AND (:keyword IS NULL OR n.title LIKE %:keyword% OR n.content LIKE %:keyword%) " +
             "ORDER BY " +
-            "CASE WHEN n.priority = 'EMERGENCY' THEN 1 ELSE 2 END, " +
+            "CASE WHEN n.priority = 'EMERGENCY' THEN 1 ELSE 2 END, " +  // 긴급 공지 우선 정렬
             "n.regAt DESC")
-    Page<Notice> findSortedNoticesForAdmin(@Param("keyword") String keyword,
+    Page<Notice> findSortedNoticesWithoutIsDeleted(@Param("keyword") String keyword,
                                                    @Param("category") Notice.Category category,
                                                    Pageable pageable);
 
