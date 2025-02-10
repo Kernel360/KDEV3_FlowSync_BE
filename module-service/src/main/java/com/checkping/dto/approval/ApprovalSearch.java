448151a0dc428415dd9a234fd247735901a3ca30
oowtl <oowtl7425@gmail.com>
2025-02-10 17:05:42 +0900

    rename : 결재 목록 조회 시 컬럼명 변경
    - 프론트 측의 요청으로 인해 좀 더 정확한 표현으로 수정
    
    관련 이슈
    - #403

--- module-service/src/main/java/com/checkping/dto/approval/ApprovalSearch.java
+++ module-service/src/main/java/com/checkping/dto/approval/ApprovalSearch.java
@@ -26,7 +26,7 @@ public class ApprovalSearch {
         register : 작성자
         regAt : 작성일
         updatedAt : 수정일
-        approvalAt : 결재일
+        approvedAt : 결재일
         approval : 결재자
         cancelAt : 취소일
         isDeleted : 삭제 여부
@@ -39,7 +39,7 @@ public class ApprovalSearch {
         private MeResponseDto register;
         private String regAt;
         private String updatedAt;
-        private String approverAt;
+        private String approvedAt;
         private MeResponseDto approver;
         private String cancelAt;
         private boolean isDeleted;
@@ -61,7 +61,7 @@ public class ApprovalSearch {
             dto.register = MeResponseDto.fromEntity(approval.getRegister());
             dto.regAt = DateTimeUtils.format(approval.getRegAt());
             dto.updatedAt = DateTimeUtils.format(approval.getUpdatedAt());
-            dto.approverAt = DateTimeUtils.format(approval.getApproverAt());
+            dto.approvedAt = DateTimeUtils.format(approval.getApproverAt());
             dto.approver = approval.getApprover() == null ? null : MeResponseDto.fromEntity(approval.getApprover());
             dto.cancelAt = DateTimeUtils.format(approval.getCancelAt());
             dto.isDeleted = approval.getDeleteYn() == Approval.DeleteStatus.Y;
