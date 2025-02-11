0a4f323ae7c1abb1cd7503e4186b44043f0e2237
a121255 <a132258@naver.com>
2025-02-11 10:13:22 +0900

    chore : 소괄호 누락
    - confilict 해결 중 소괄호 누락

--- module-api/src/main/java/com/checkping/api/controller/ProjectApi.java
+++ module-api/src/main/java/com/checkping/api/controller/ProjectApi.java
@@ -69,6 +69,7 @@ public interface ProjectApi {
     BaseResponse<ProjectResponse.ProjectDto> updateProjectsByManagementSteps(
             @Parameter(description = "프로젝트 ID") Long projectId,
             @Parameter(description = "프로젝트 관리단계") String managementStep
+    );
 
     @Operation(summary = "프로젝트 진행 상태 일정 수정", description = "프로젝트 진행 상태의 일정을 수정하는 기능입니다.")
     BaseResponse<ProgressStepPlanUpdate.Response> updateProgressStepPlan(
