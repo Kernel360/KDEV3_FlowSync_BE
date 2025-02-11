96338286dc65194dc054a1d82c424ed3bfee24da
oowtl <oowtl7425@gmail.com>
2025-02-11 17:02:08 +0900

    feat : 권한 체크 시 예외 처리
    - ApprovalMismatchProjectException : 프로젝트에 해당하는 결재가 아님
    - ApprovalUpdaterAuthorityException : 결재를 수정할 수 있는 권한이 없음
    - ProjectNotCustomerOwnerException : 고객사 최고 담당자 권한이 없음
    - ProjectNotDevOwnerException : 개발사 최고 담당자 권한이 없음
    - ProjectNotMemberException : 프로젝트 진행 중인 회원이 아님
    
    관련 이슈
    - #395

--- module-service/src/main/java/com/checkping/exception/approval/ApprovalMismatchProjectException.java
+++ module-service/src/main/java/com/checkping/exception/approval/ApprovalMismatchProjectException.java
@@ -2,9 +2,9 @@ package com.checkping.exception.approval;
 
 import com.checkping.common.enums.ErrorCode;
 
-public class ApprovalMismatchException extends ApprovalException {
+public class ApprovalMismatchProjectException extends ApprovalException {
 
-    public ApprovalMismatchException() {
+    public ApprovalMismatchProjectException() {
         super("해당 프로젝트에 속한 결재 글이 아닙니다.", ErrorCode.BAD_REQUEST);
     }
 }
