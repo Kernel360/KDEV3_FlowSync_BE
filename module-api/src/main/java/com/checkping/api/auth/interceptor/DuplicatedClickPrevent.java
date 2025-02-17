d905c7c70b4c2bf65aab5449da1245dd4019c5fc
heegu123 <kookminkhg@kookmin.ac.kr>
2025-02-17 21:57:59 +0900

    feat: 중복 요청 방지를 위한 인터셉터 `DuplicatedClickPrevent` 추가
    - `HandlerInterceptor`를 구현하여 중복 클릭 방지 기능 추가
    - Redis를 활용하여 `setIfAbsent()` 방식으로 중복 요청 차단 (TTL 3초)
    - GET 요청은 예외적으로 통과하도록 처리
    - `memberId`를 기반으로 요청 식별 및 중복 체크 수행
    - 중복 요청 발생 시 `DuplicateRequestException` 예외 발생
    
    #Issue: 508

---
+++ module-api/src/main/java/com/checkping/api/auth/interceptor/DuplicatedClickPrevent.java
@@ -0,0 +1,59 @@
+package com.checkping.api.auth.interceptor;
+
+import com.checkping.exception.request.DuplicateRequestException;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.data.redis.core.StringRedisTemplate;
+import org.springframework.stereotype.Component;
+import org.springframework.web.context.request.RequestContextHolder;
+import org.springframework.web.context.request.ServletRequestAttributes;
+import org.springframework.web.servlet.HandlerInterceptor;
+
+import java.util.concurrent.TimeUnit;
+
+@Component
+@Slf4j
+public class DuplicatedClickPrevent implements HandlerInterceptor {
+
+    private final StringRedisTemplate stringRedisTemplate;
+    private static final String PREFIX = "CLICK:ID:";
+
+    public DuplicatedClickPrevent(@Qualifier("redisTemplateForDuplicatedClick") StringRedisTemplate stringRedisTemplate) {
+        this.stringRedisTemplate = stringRedisTemplate;
+    }
+
+    public boolean checkAndSetRequest(String email) {
+        String key = PREFIX + email;
+        Boolean success = stringRedisTemplate.opsForValue()
+                .setIfAbsent(key, "LOCK", 3, TimeUnit.SECONDS);
+
+        return success != null && success;
+    }
+
+    @Override
+    public boolean preHandle(HttpServletRequest request, HttpServletResponse response, Object handler) throws Exception {
+        String requestURI = request.getRequestURI();
+        String method = request.getMethod();
+
+        // 1. GET 요청이면 바로 통과
+        if ("GET".equalsIgnoreCase(method)) {
+            return true;
+        }
+
+        Object memberId = ((ServletRequestAttributes) RequestContextHolder.getRequestAttributes())
+                .getRequest().getAttribute("memberId");
+
+        if (memberId == null) {
+            log.warn("member ID가 없습니다. 중복 체크를 패스합니다.");
+            return true; // memberId가 없으면 중복 체크 패스
+        }
+
+        if(!checkAndSetRequest(memberId.toString())) {
+            throw new DuplicateRequestException();
+        }
+
+        return true;
+    }
+}
