8a8e867d17aaefe79fcb9ea2950dce451e6754e6
jaseongkim <jaseong4281@gmail.com>
2025-02-18 16:25:30 +0900

    refactor: 업체 modify 수정
    파일수정 로직 변경
    -> request URL 기존 URl로 변경
    -> 파일 여부 체크 후 기존 파일 삭제
    
    Issue #525

--- module-service/src/main/java/com/checkping/service/member/OrganizationServiceImpl.java
+++ module-service/src/main/java/com/checkping/service/member/OrganizationServiceImpl.java
@@ -111,18 +111,21 @@ public class OrganizationServiceImpl implements OrganizationService {
         Optional<Organization> result = organizationRepository.findById(id);
         Organization organization = result.orElseThrow(OrganizationNotFoundEntityException::new);
 
-        // 기존 파일이 있다면 삭제
-        if (organization.getBrCertificateUrl() != null && !organization.getBrCertificateUrl().isEmpty()) {
-            // 저장 파일명
-            String saveName = organization.getBrCertificateUrl().split("\\|")[0];
-            // 기존 파일 삭제
-            s3FileRepository.deleteFile(saveName);
-        }
+        // 프론트에서 파일을 수정하지 않고 보내면 url에 Prisigned URL을 보내기 때문에 기존 URL로 수정 작업
+        request.setBrCertificateUrl(organization.getBrCertificateUrl());
 
         // 수정 파일 등록
         if (file != null) {
             FileRequest fileRequest = s3FileRepository.uploadFile(file);
             request.setBrCertificateUrl(fileRequest.saveName() + "|" + fileRequest.url());
+
+            // 기존 파일이 있다면 삭제
+            if (organization.getBrCertificateUrl() != null && organization.getBrCertificateUrl().contains("|")) {
+                // 저장 파일명
+                String saveName = organization.getBrCertificateUrl().split("\\|")[0];
+                // 기존 파일 삭제
+                s3FileRepository.deleteFile(saveName);
+            }
         }
 
         organization.updateOrganization(
